362287253fe13887b501b7b870369f68da67d32a
Arun Mahapatra <armahapa@microsoft.com>
2016-08-12 11:55:26 +0530

    Address PR Comments.
    Add standard job setup. Use github pr/push triggers.

--- netci.groovy
+++ netci.groovy
@@ -9,49 +9,29 @@ def project = GithubProject
 [true, false].each { isPR -> // Defines a closure over true and false, value assigned to isPR
     ['Debug', 'Release'].each { configuration ->
         
-        // Determine the name for the new job.  The first parameter is the project,
-        // the second parameter is the base name for the job, and the last parameter
-        // is a boolean indicating whether the job will be a PR job.  If true, the
-        // suffix _prtest will be appended.
+        // Determine the name for the new job. A _prtest suffix is appended if isPR is true.
         def newJobName = InternalUtilities.getFullJobName(project, configuration, isPR)
         
         // Define your build/test strings here
-        def buildString = """call \"C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\Common7\\Tools\\VsDevCmd.bat\" && build.cmd -c ${configuration}"""
+        def buildString = """call build.cmd -c ${configuration}"""
 
-        // Create a new job with the specified name.  The brace opens a new closure
-        // and calls made within that closure apply to the newly created job.
+        // Create a new job for windows build
         def newJob = job(newJobName) {
-            // Indicate this job runs on windows machines.
-            // The valid labels right now are:
-            // windows (server 2012 R2)
-            // ubuntu (v. 14.04)
-            // centos-71
-            // openSuSE-132
-            // mac
-            // freebsd
-            label('windows')
-            
-            // This opens the set of build steps that will be run.
             steps {
-                // Indicates that a batch script should be run with the build string (see above)
-                // Also available is:
-                // shell (for unix scripting)
                 batchFile(buildString)
             }
         }
+
+        Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto-internal')
         
         // This call performs remaining common job setup on the newly created job.
-        // This is used most commonly for simple inner loop testing.
-        // It does the following:
-        //   1. Sets up source control for the project.
-        //   2. Adds a push trigger if the job is a PR job
-        //   3. Adds a github PR trigger if the job is a PR job.
-        //      The optional context (label that you see on github in the PR checks) is added.
-        //      If not provided the context defaults to the job name.
-        //   4. Adds standard options for build retention and timeouts
-        //   5. Adds standard parameters for PR and push jobs.
-        //      These allow PR jobs to be used for simple private testing, for instance.
-        // See the documentation for this function to see additional optional parameters.
-        InternalUtilities.simpleInnerLoopJobSetup(newJob, project, isPR, "Windows ${configuration}")
+        InternalUtilities.standardJobSetup(newJob, project, isPR, "*/${branchName}")
+
+        if (isPR) {
+            Utilities.addGithubPRTriggerForBranch(newJob, branch, "Windows / ${os} ${configuration} Build")
+        }
+        else {
+            Utilities.addGithubPushTrigger(newJob)
+        }
     }
 }
