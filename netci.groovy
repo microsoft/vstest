dfba488f76f843f12cc9d434b80918b30c83d584
Arun Mahapatra <codito@users.noreply.github.com>
2016-08-12 13:28:46 +0530

    Add netci.groovy for CI jobs. (#10)
    * Add netci.groovy for CI jobs.
    * Address PR Comments.
    * Add standard job setup. Use github pr/push triggers.
    * Add branch name property.
    * Fix typo: remove OS from PR trigger.

---
+++ netci.groovy
@@ -0,0 +1,38 @@
+// Import the utility functionality.
+import jobs.generation.Utilities;
+import jobs.generation.InternalUtilities;
+
+// Defines a the new of the repo, used elsewhere in the file
+def project = GithubProject
+def branch = GithubBranchName
+
+// Generate the builds for debug and release, commit and PRJob
+[true, false].each { isPR -> // Defines a closure over true and false, value assigned to isPR
+    ['Debug', 'Release'].each { configuration ->
+        
+        // Determine the name for the new job. A _prtest suffix is appended if isPR is true.
+        def newJobName = InternalUtilities.getFullJobName(project, configuration, isPR)
+        
+        // Define your build/test strings here
+        def buildString = """call build.cmd -c ${configuration}"""
+
+        // Create a new job for windows build
+        def newJob = job(newJobName) {
+            steps {
+                batchFile(buildString)
+            }
+        }
+
+        Utilities.setMachineAffinity(newJob, 'Windows_NT', 'latest-or-auto-internal')
+        
+        // This call performs remaining common job setup on the newly created job.
+        InternalUtilities.standardJobSetup(newJob, project, isPR, "*/${branch}")
+
+        if (isPR) {
+            Utilities.addGithubPRTriggerForBranch(newJob, branch, "Windows / ${configuration} Build")
+        }
+        else {
+            Utilities.addGithubPushTrigger(newJob)
+        }
+    }
+}
