7322c3596a8bf9de0103717eb98b5211ca5b02ae
Faizan Ahmad <faahmad@microsoft.com>
2016-08-29 19:51:39 +0530

    Fixed build issue.

--- test/vstest.console.UnitTests/Processors/Utilities/ArgumentProcessorFactoryTests.cs
+++ test/vstest.console.UnitTests/Processors/Utilities/ArgumentProcessorFactoryTests.cs
@@ -3,7 +3,6 @@
 namespace Microsoft.VisualStudio.TestPlatform.CommandLine.UnitTests.Processors.Utilities
 {
     using System;
-    using System.Collections;
     using System.Collections.Generic;
     using System.Collections.Immutable;
     using System.Linq;
