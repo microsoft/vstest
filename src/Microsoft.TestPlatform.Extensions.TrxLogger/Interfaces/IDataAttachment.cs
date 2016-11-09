4fff25f570fbbf2f6cb5b66997ebfcbbe96974e1
Arun Mahapatra <armahapa@microsoft.com>
2016-11-08 19:37:25 -0600

    Update license header.

--- src/Microsoft.TestPlatform.Extensions.TrxLogger/Interfaces/IDataAttachment.cs
+++ src/Microsoft.TestPlatform.Extensions.TrxLogger/Interfaces/IDataAttachment.cs
@@ -1,5 +1,7 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.TestPlatform.Extensions.TrxLogger.ObjectModel
 {
