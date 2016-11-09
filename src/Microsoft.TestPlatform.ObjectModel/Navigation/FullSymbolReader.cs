760d1c0b12309ea287f5948f2c9ed9de03ad02c0
Arun Mahapatra <codito@users.noreply.github.com>
2016-11-09 21:52:35 +0530

    Update FullSymbolReader.cs

--- src/Microsoft.TestPlatform.ObjectModel/Navigation/FullSymbolReader.cs
+++ src/Microsoft.TestPlatform.ObjectModel/Navigation/FullSymbolReader.cs
@@ -1,5 +1,6 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
 namespace Microsoft.VisualStudio.TestPlatform.ObjectModel.Navigation
 {
 #if NET46
