b7e69fff696a563e901d2439ab881e86651aa24c
Faizan2304 <faizan2304@hotmail.com>
2016-11-21 01:16:21 +0530

    test platform side fix for issue https://github.com/Microsoft/vstest/issues/192 (#232)
    * test platform side fix for issue https://github.com/Microsoft/vstest/issues/192
    
    * Fix for issue https://github.com/Microsoft/vstest/issues/202
    
    * Removing unusefull double quotes
    
    * Addressed PR comment
    
    * Renamed the task name

---
+++ src/Microsoft.TestPlatform.Build/Tasks/PrintBuildStartEndTask.cs
@@ -0,0 +1,35 @@
+﻿// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.TestPlatform.Build.Tasks
+{
+    using System.Collections.Generic;
+
+    using Microsoft.Build.Framework;
+    using Microsoft.Build.Utilities;
+    using System;
+    using System.IO;
+
+    public class BuildLogTask : Task
+    {
+        public bool BuildStarted
+        {
+            get;
+            set;
+        }
+
+        public override bool Execute()
+        {
+            if (BuildStarted)
+            {
+                Console.WriteLine("Build started, please wait...");
+            }
+            else
+            {
+                Console.WriteLine("Build completed.");
+                Console.WriteLine();
+            }
+            return true;
+        }
+    }
+}
