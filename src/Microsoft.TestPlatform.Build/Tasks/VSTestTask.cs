b1785f7ae137f1d546a3128ad32d171aabb6c3af
Faizan Ahmad <faahmad@microsoft.com>
2016-11-23 12:24:45 +0530

    Dont pass AnyCPU to vstest.console

--- src/Microsoft.TestPlatform.Build/Tasks/VSTestTask.cs
+++ src/Microsoft.TestPlatform.Build/Tasks/VSTestTask.cs
@@ -101,7 +101,7 @@ namespace Microsoft.TestPlatform.Build.Tasks
                 allArgs.Add("--framework:" + this.AddDoubleQuotes(this.VSTestFramework));
             }
 
-            if (!string.IsNullOrEmpty(this.VSTestPlatform))
+            if (!string.IsNullOrEmpty(this.VSTestPlatform) && !this.VSTestPlatform.Contains("AnyCPU"))
             {
                 allArgs.Add("--platform:" + this.VSTestPlatform);
             }
