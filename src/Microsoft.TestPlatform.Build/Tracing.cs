371f8623651c132db5aa613aff0486d8b4de69e1
Faizan Ahmad <faahmad@microsoft.com>
2016-11-25 11:16:45 +0530

    Fix for issue: https://github.com/Microsoft/vstest/issues/199
    Validation: Manully validated on windows and linux machine

---
+++ src/Microsoft.TestPlatform.Build/Tracing.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Microsoft.TestPlatform.Build.Trace
+{
+    public static class Tracing
+    {
+        public static bool traceEnabled = false;
+        public static void Trace(string message)
+        {
+            if (traceEnabled)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
