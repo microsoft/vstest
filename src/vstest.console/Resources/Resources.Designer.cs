8aeff629b26ed514d1ef937568830fd7fbae7821
Arun Mahapatra <armahapa@microsoft.com>
2016-11-28 08:57:02 +0530

    Support discovery of testhost from test project output directory. Fix resources for vstest.console.

--- src/vstest.console/Resources/Resources.Designer.cs
+++ src/vstest.console/Resources/Resources.Designer.cs
@@ -40,7 +40,7 @@ namespace Microsoft.VisualStudio.TestPlatform.CommandLine.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.VisualStudio.TestPlatform.CommandLine.Resources.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("vstest.console.Resources.Resources", typeof(Resources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
