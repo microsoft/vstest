230c966318ce4960b9729b2e3e154bdb3572abf6
Sarabjot Singh <sasin@microsoft.com>
2017-01-03 18:16:03 +0530

    First Draft for the Protocol tool. (#306)
    * First Draft for the Protocol tool.
    Emits the json sent and recieved for Discovery, RunAll, RunSelected scenarios.
    
    * Fixing the Testplatform.sln build.
    
    * Changes as per discussion with Arun.
    - Removed the runnerlocation, adapter and testhost configs.

--- samples/UnitTestProject/UnitTest.cs
+++ samples/UnitTestProject/UnitTest.cs
@@ -8,9 +8,62 @@ namespace UnitTestProject
     [TestClass]
     public class UnitTest
     {
+        /// <summary>
+        /// The passing test.
+        /// </summary>
         [TestMethod]
-        public void TestMethod1()
+        public void PassingTest()
         {
+            Assert.AreEqual(2, 2);
+        }
+
+        /// <summary>
+        /// The failing test.
+        /// </summary>
+        [TestMethod]
+        public void FailingTest()
+        {
+            Assert.AreEqual(2, 3);
+        }
+
+        /// <summary>
+        /// The skipping test.
+        /// </summary>
+        [Ignore]
+        [TestMethod]
+        public void SkippingTest()
+        {
+        }
+
+        [TestCategory("CategoryA")]
+        [TestMethod]
+        public void TestWithTestCategory()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [Priority(0)]
+        [TestMethod]
+        public void TestWithPriority()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestProperty("Property1", "Value1")]
+        [TestProperty("Property2", "Value2")]
+        [TestMethod]
+        public void TestWithProperties()
+        {
+            Assert.AreEqual(1, 1);
+        }
+
+        [TestCategory("CategoryA")]
+        [Priority(1)]
+        [TestProperty("Property2", "Value2")]
+        [TestMethod]
+        public void FailingTestWithTraits()
+        {
+            Assert.Fail();
         }
     }
 }
