8c179a76b3eca849a48fe2503ceef5f8c44b13dd
Faizan2304 <faizan2304@hotmail.com>
2017-01-06 02:16:13 +0530

    Convert TestPlatform.vsix from V2 to V3 format. (#316)
    * converting TestPlatform.vsix from V2 format to V3 format

--- src/Microsoft.TestPlatform.VSIXCreator/Program.cs
+++ src/Microsoft.TestPlatform.VSIXCreator/Program.cs
@@ -5,6 +5,7 @@ namespace Microsoft.TestPlatform.VSIXCreator
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.IO.Compression;
     using System.Linq;
     using System.Threading.Tasks;
@@ -15,7 +16,7 @@ namespace Microsoft.TestPlatform.VSIXCreator
         {
             var inputDirectory = "win7-x64";
             var outputDirectory = System.Environment.CurrentDirectory;
-            if(args.Length > 0 && !String.IsNullOrEmpty(args[0]))
+            if (args.Length > 0 && !String.IsNullOrEmpty(args[0]))
             {
                 inputDirectory = args[0];
             }
@@ -31,10 +32,26 @@ namespace Microsoft.TestPlatform.VSIXCreator
                 System.IO.File.Delete(vsixFilePath);
             }
 
-            if(System.IO.Directory.Exists(inputDirectory))
+            if (System.IO.Directory.Exists(inputDirectory))
             {
-                ZipFile.CreateFromDirectory(inputDirectory, vsixFilePath, CompressionLevel.Fastest, false);
-            }      
+                // Get all files to put in vsix
+                IEnumerable<string> files = Directory.EnumerateFiles(inputDirectory, "*.*", SearchOption.AllDirectories);
+                int inputDirectoryLength = inputDirectory.Length;
+
+                using (ZipArchive vsixFile = ZipFile.Open(vsixFilePath, ZipArchiveMode.Create))
+                {
+                    foreach (var file in files)
+                    {
+                        if (!file.EndsWith(".pdb"))
+                        {
+                            // Creating same directory structure as of inputDirectory and replacing \ with / to make compatible with V3 format.
+                            // because if we create vsix with \ then at time of installation sub folder does not get copied.
+                            string addFile = file.Substring(inputDirectoryLength + 1).Replace(@"\", @"/");
+                            vsixFile.CreateEntryFromFile(file, addFile, CompressionLevel.Optimal);
+                        }
+                    }
+                }
+            }
         }
     }
 }
