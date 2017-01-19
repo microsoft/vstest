4240112ebcf8509c725661e616ed58a07cd58b44
Sarabjot Singh <sasin@microsoft.com>
2017-01-19 07:29:22 -0800

    Working directory fix (#346)
    * Fixed the working directory for the dotnet.exe to source directory.
    
    * Adding the acceptance test for the working directory fix.

--- test/TestAssets/SimpleTestProject3/UnitTest1.cs
+++ test/TestAssets/SimpleTestProject3/UnitTest1.cs
@@ -5,10 +5,16 @@ using System.Threading.Tasks;
 namespace SampleUnitTestProject3
 {
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.IO;
+    using System.Reflection;
 
     [TestClass]
     public class UnitTest1
     {
-        
+        [TestMethod]
+        public void WorkingDirectoryTest()
+        {
+            Assert.AreEqual(Path.GetDirectoryName(typeof(UnitTest1).GetTypeInfo().Assembly.Location), Directory.GetCurrentDirectory());
+        }
     }
 }
