01a1abfa727c3a76a3c01324432eae4bda584bfe
Faizan2304 <faizan2304@hotmail.com>
2017-02-02 01:31:53 -0800

    Replace invalid xml character with ? (#425)
    * Issue: https://github.com/Microsoft/vstest/issues/320
    Fix: Replace invalid xml character with ?

---
+++ test/Microsoft.TestPlatform.Extensions.TrxLogger.UnitTests/XmlPersistenceTests.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.TestPlatform.Extensions.TrxLogger.UnitTests
+{
+    using Microsoft.TestPlatform.Extensions.TrxLogger.XML;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System.IO;
+
+    [TestClass]
+    public class XmlPersistenceTests
+    {
+        [TestMethod]
+        public void SaveObjectShouldRemoveInvalidCharacter()
+        {
+            XmlPersistence xmlPersistence = new XmlPersistence();
+            var node = xmlPersistence.CreateRootElement("TestRun");
+
+            string strWithInvalidCharForXml = "This string has these \0 \v invalid characters";
+
+            xmlPersistence.SaveObject(strWithInvalidCharForXml, node, null, "dummy");
+
+            string expectedResult = "This string has these ? ? invalid characters";
+            Assert.AreEqual(string.Compare(expectedResult, node.InnerXml), 0);
+        }
+
+        [TestMethod]
+        public void SaveObjectShouldDoesNotRemoveValidCharacter()
+        {
+            XmlPersistence xmlPersistence = new XmlPersistence();
+            var node = xmlPersistence.CreateRootElement("TestRun");
+
+            string strWithInvalidCharForXml = "This string has these \\0 \v invalid characters";
+
+            xmlPersistence.SaveObject(strWithInvalidCharForXml, node, null, "dummy");
+
+            string expectedResult = "This string has these \\0 ? invalid characters";
+            Assert.AreEqual(string.Compare(expectedResult, node.InnerXml), 0);
+        }
+    }
+}
