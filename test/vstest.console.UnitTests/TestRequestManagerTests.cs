64d1f5ce1e5874cd0bc7f798976d77920a1af2cd
Abhitej Anoop John Bandi <aajohn@microsoft.com>
2017-02-12 20:16:06 +0530

    Tests and rename.

--- test/vstest.console.UnitTests/TestRequestManagerTests.cs
+++ test/vstest.console.UnitTests/TestRequestManagerTests.cs
@@ -3,12 +3,19 @@
 
 namespace Microsoft.VisualStudio.TestPlatform.CommandLine.UnitTests
 {
+    using System;
+    using System.Collections.Generic;
+
+    using Microsoft.VisualStudio.TestPlatform.Client.RequestHelper;
     using Microsoft.VisualStudio.TestPlatform.CommandLine;
     using Microsoft.VisualStudio.TestPlatform.CommandLine.TestPlatformHelpers;
+    using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
     using Microsoft.VisualStudio.TestPlatform.Common.Logging;
     using Microsoft.VisualStudio.TestPlatform.CoreUtilities.Tracing.Interfaces;
     using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    
+
     using Moq;
     using vstest.console.UnitTests.TestDoubles;
 
@@ -22,6 +29,12 @@ namespace Microsoft.VisualStudio.TestPlatform.CommandLine.UnitTests
         {
             this.mockLoggerEvents = new DummyLoggerEvents(TestSessionMessageLogger.Instance);
             this.mockLoggerManager = new DummyTestLoggerManager(this.mockLoggerEvents);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            CommandLineOptions.Instance.Reset();
         }
 
         [TestMethod]
@@ -48,6 +61,39 @@ namespace Microsoft.VisualStudio.TestPlatform.CommandLine.UnitTests
                 new Mock<ITestPlatformEventSource>().Object);
 
             Assert.IsFalse(mockLoggerEvents.EventsSubscribed());
+        }
+
+        [TestMethod]
+        public void DiscoverTestsShouldHonorStatsConfiguration()
+        {
+            // Arrange.
+            long batchSize = 512;
+            var testStatsTimeout = new TimeSpan(0, 0, 0, 59, 0);
+
+            CommandLineOptions.Instance.BatchSize = batchSize;
+            CommandLineOptions.Instance.TestStatsEventTimeout = testStatsTimeout;
+
+            DiscoveryCriteria receivedCriteria = null;
+            var mockTestPlatform = new Mock<ITestPlatform>();
+
+            // Setup.
+            mockTestPlatform.Setup(tp => tp.CreateDiscoveryRequest(It.IsAny<DiscoveryCriteria>()))
+                .Returns(new Mock<IDiscoveryRequest>().Object)
+                .Callback((DiscoveryCriteria criteria) => { receivedCriteria = criteria; });
+
+            var requestManager = new TestRequestManager(CommandLineOptions.Instance,
+                mockTestPlatform.Object,
+                this.mockLoggerManager,
+                TestRunResultAggregator.Instance,
+                new Mock<ITestPlatformEventSource>().Object);
+
+            // Act.
+            requestManager.DiscoverTests(new DiscoveryRequestPayload() { Sources = new List<string> { "C:\\somerandomfile.foo" } }, new Mock<ITestDiscoveryEventsRegistrar>().Object);
+
+            // Assert.
+            Assert.IsNotNull(receivedCriteria);
+            Assert.AreEqual(batchSize, receivedCriteria.FrequencyOfDiscoveredTestsEvent);
+            Assert.AreEqual(testStatsTimeout, receivedCriteria.DiscoveredTestEventTimeout);
         }
     }
 }
