368145d1fedde428783edaf4d6e0646032134996
Harsh Jain <harshjain.nitkkr@gmail.com>
2017-04-16 16:49:52 +0530

    Use latest version of dotnet. (#678)
    * Use latest version of dotnet.
    
    * Try 1.0.1 version of stylecop.analyzers. It packages the netcoreapp versions of newtonsoft json.
    
    * Download netcoreapp1.0 shared runtime since it is no longer shipped with dotnet-sdk 2.0.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -61,8 +61,7 @@ $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
 # Dotnet build doesn't support --packages yet. See https://github.com/dotnet/cli/issues/2712
 $env:NUGET_PACKAGES = $env:TP_PACKAGES_DIR
 $env:NUGET_EXE_Version = "3.4.3"
-# Dotnet build is not supporting -p: to pass arguments to msbuild. Revert to "latest" after the issue is resolved. See https://github.com/dotnet/cli/issues/6124
-$env:DOTNET_CLI_VERSION = "2.0.0-preview1-005448"
+$env:DOTNET_CLI_VERSION = "latest"
 $env:LOCATE_VS_API_VERSION = "0.2.4-beta"
 $env:MSBUILD_VERSION = "15.0"
 
@@ -133,8 +132,11 @@ function Install-DotNetCli
     New-Item -ItemType directory -Path $dotnetInstallPath -Force | Out-Null
     & $dotnetInstallScript -Channel "master" -InstallDir $dotnetInstallPath -NoPath -Version $env:DOTNET_CLI_VERSION
 
-    # Uncomment to pull in additional shared frameworks.
-    # This is added to get netcoreapp1.1 shared components.
+    # Pull in additional shared frameworks.
+    # Get netcoreapp1.0 shared components.
+    & $dotnetInstallScript -InstallDir $dotnetInstallPath -SharedRuntime -Version '1.0.4' -Channel 'preview'
+    
+    # Get netcoreapp1.1 shared components.
     & $dotnetInstallScript -InstallDir $dotnetInstallPath -SharedRuntime -Version '1.1.1' -Channel 'release/1.1.0'
     
     Write-Log "Install-DotNetCli: Complete. {$(Get-ElapsedTime($timer))}"
