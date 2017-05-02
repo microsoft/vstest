67e8d815f237b983d9ca931af6e862b9ddb9e815
Faizan Ahmad <faahmad@microsoft.com>
2017-05-02 03:05:16 -0700

    nitpick

--- src/Microsoft.TestPlatform.CoreUtilities/Utilities/StringExtensions.cs
+++ src/Microsoft.TestPlatform.CoreUtilities/Utilities/StringExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.VisualStudio.TestPlatform.CoreUtilities.Extensions;
+namespace Microsoft.VisualStudio.TestPlatform.CoreUtilities.Extensions
 {
     public static class StringExtensions
     {
