6d07f2a4b1b0a8dc2461d9695e2dd9753694d1f3
Faizan Ahmad <faahmad@microsoft.com>
2017-05-10 06:36:13 -0700

    Issue: 1) https://github.com/Microsoft/vstest/issues/706 2) https://github.com/Microsoft/vstest/issues/618
    Fix:
    Don’t serialize the type if that have self-referencing loop

---
+++ test/Microsoft.TestPlatform.CommunicationUtilities.UnitTests/JsonDataSerializerTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.TestPlatform.CommunicationUtilities.UnitTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class JsonDataSerializerTests
+    {
+        [TestMethod]
+        public void SerializePayloadShouldSerializeAnObjectWithSelfReferencingLoop()
+        {
+            var classWithSelfReferencingLoop = new ClassWithSelfReferencingLoop(null);
+            classWithSelfReferencingLoop = new ClassWithSelfReferencingLoop(classWithSelfReferencingLoop);
+            classWithSelfReferencingLoop.InfiniteRefernce.InfiniteRefernce = classWithSelfReferencingLoop;
+
+            var sut = JsonDataSerializer.Instance;
+
+            // this line should not throw exception
+            sut.SerializePayload("dummy", classWithSelfReferencingLoop);
+        }
+
+        public class ClassWithSelfReferencingLoop
+        {
+            public ClassWithSelfReferencingLoop(ClassWithSelfReferencingLoop ir)
+            {
+                this.InfiniteRefernce = ir;
+            }
+
+            public ClassWithSelfReferencingLoop InfiniteRefernce
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
