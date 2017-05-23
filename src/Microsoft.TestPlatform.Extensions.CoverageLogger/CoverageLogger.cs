f523828f57f867d258e1a97013aaf18d84c1b303
Mayank Bansal <maban@microsoft.com>
2017-05-23 16:04:32 +0530

    CodeCoverage logger

---
+++ src/Microsoft.TestPlatform.Extensions.CoverageLogger/CoverageLogger.cs
@@ -0,0 +1,259 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TestPlatform.Extensions.CoverageLogger
+{
+    using System;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading;
+    using System.Xml;
+    using Microsoft.TestPlatform.COM;
+    using Microsoft.VisualStudio.Setup.Interop;
+
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
+    using Microsoft.VisualStudio.TestPlatform.Utilities;
+
+    using CodeCoverageLoggerResources = Microsoft.TestPlatform.Extensions.Coverage.TestLogger.Resources.CoverageResource;
+
+    /// <summary>
+    /// Logger for Generating CodeCoverage Analysis
+    /// </summary>
+    [FriendlyName(FriendlyName)]
+    [ExtensionUri(CoverageUri)]
+    internal class CoverageLogger : ITestLogger
+    {
+        #region Constants
+
+        private const string CoverageUri = "datacollector://microsoft/CodeCoverage/2.0";
+
+        private const string FriendlyName = "CoverageLogger";
+
+        private const string CodeCoverageExeRelativePath = @"Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe";
+
+        private const string SetupInteropx86 = @"x86\Microsoft.VisualStudio.Setup.Configuration.Native.dll";
+
+        private const string SetupInteropx64 = @"x64\Microsoft.VisualStudio.Setup.Configuration.Native.dll";
+
+        private static readonly Uri CodeCoverageDataCollectorUri = new Uri(CoverageUri);
+
+        private Process vanguardProcess;
+
+        private ManualResetEvent coverageXmlGenerateEvent;
+
+        #endregion
+
+        public static XmlReaderSettings ReaderSettings => new XmlReaderSettings
+                                                              {
+                                                                  IgnoreComments = true,
+                                                                  IgnoreWhitespace = true,
+                                                                  DtdProcessing = DtdProcessing
+                                                                      .Prohibit
+                                                              };
+
+        #region ITestLogger
+
+        /// <inheritdoc/>
+        public void Initialize(TestLoggerEvents events, string testResultsDirPath)
+        {
+            if (events == null)
+            {
+                throw new ArgumentNullException(nameof(events));
+            }
+
+            // Register for the events.
+            events.TestRunComplete += this.TestRunCompleteHandler;
+
+            this.coverageXmlGenerateEvent = new ManualResetEvent(false);
+        }
+        #endregion
+
+        #region Event Handlers
+
+        /// <summary>
+        /// Called when a test run is completed.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// Test run complete events arguments.
+        /// </param>
+        internal void TestRunCompleteHandler(object sender, TestRunCompleteEventArgs e)
+        {
+            if (e.AttachmentSets == null)
+            {
+                return;
+            }
+
+            var coverageAttachments = e.AttachmentSets
+                .Where(dataCollectionAttachment => CodeCoverageDataCollectorUri.Equals(dataCollectionAttachment.Uri)).ToArray();
+
+            if (coverageAttachments.Any())
+            {
+                var codeCoverageFiles = coverageAttachments.Select(coverageAttachment => coverageAttachment.Attachments[0].Uri.LocalPath).ToArray();
+
+                foreach (var codeCoverageFile in codeCoverageFiles)
+                {
+                    var resultFile = Path.Combine(Path.GetDirectoryName(codeCoverageFile), Path.GetFileNameWithoutExtension(codeCoverageFile) + ".xml");
+                    var arguments = "analyze /output:" + '"' + resultFile + '"' + " " + '"' + codeCoverageFile + '"';
+
+                    this.AnalyzeCoverageFile(arguments);
+
+                    if (this.coverageXmlGenerateEvent.WaitOne(10000))
+                    {
+                        ConsoleOutput.Instance.Information(resultFile);
+
+                        this.ProcessCoverageXml(resultFile);
+                    }
+                    else
+                    {
+                        EqtTrace.Error("CodeCoverage.exe could not process file within 10 secs");
+                        if (!this.vanguardProcess.HasExited)
+                        {
+                            this.vanguardProcess.Kill();
+                        }
+                    }
+
+                    this.vanguardProcess.Dispose();
+                }
+            }
+        }
+
+        private void ProcessCoverageXml(string coverageXml)
+        {
+            using (XmlReader reader = XmlReader.Create(coverageXml, ReaderSettings))
+            {
+                var settingsDocument = new XmlDocument();
+                settingsDocument.Load(reader);
+
+                var modules = settingsDocument.GetElementsByTagName("module");
+
+                if (modules.Count > 0)
+                {
+                    ConsoleOutput.Instance.Information("\n" + string.Format(CultureInfo.CurrentCulture, CodeCoverageLoggerResources.CodeCoverageHeader));
+                    ConsoleOutput.Instance.Information("----------------");
+
+                    var header = string.Concat(
+                        string.Format(CultureInfo.CurrentCulture, CodeCoverageLoggerResources.ModuleName),
+                        "\t\t\t",
+                        string.Format(CultureInfo.CurrentCulture, CodeCoverageLoggerResources.NotBlocksCovered),
+                        "\t\t",
+                        string.Format(CultureInfo.CurrentCulture, CodeCoverageLoggerResources.PercNotBlocksCovered),
+                        "\t\t",
+                        string.Format(CultureInfo.CurrentCulture, CodeCoverageLoggerResources.BlocksCovered),
+                        "\t\t",
+                        string.Format(CultureInfo.CurrentCulture, CodeCoverageLoggerResources.PercBlocksCovered),
+                        "\n");
+
+                    ConsoleOutput.Instance.Information(header);
+                }
+
+                foreach (XmlNode moudle in modules)
+                {
+                    ConsoleOutput.Instance.Information(moudle.Attributes["name"].Value + "\t\t" +
+                                                       moudle.Attributes["blocks_not_covered"].Value + "\t\t\t\t\t" +
+                                                       (100 - float.Parse(moudle.Attributes["block_coverage"].Value)) + "\t\t\t\t" +
+                                                       moudle.Attributes["blocks_covered"].Value + "\t\t\t\t" +
+                                                       moudle.Attributes["block_coverage"].Value);
+                }
+            }
+        }
+
+        private void AnalyzeCoverageFile(string arguments)
+        {
+            try
+            {
+                string vsInstallPath = this.GetVSInstallPath();
+                this.coverageXmlGenerateEvent.Reset();
+                string codeCoverageExe = Path.Combine(vsInstallPath, CodeCoverageExeRelativePath);
+
+                if (!File.Exists(codeCoverageExe))
+                {
+                    throw new FileNotFoundException(codeCoverageExe);
+                }
+
+                this.vanguardProcess = new Process
+                                           {
+                                               StartInfo =
+                                                   {
+                                                       UseShellExecute = false,
+                                                       CreateNoWindow = true,
+                                                       FileName = codeCoverageExe,
+                                                       Arguments = arguments,
+                                                       RedirectStandardError = true
+                                                   },
+                                               EnableRaisingEvents = true
+                                           };
+
+                this.vanguardProcess.Exited += this.CodeCoverageExited;
+                this.vanguardProcess.Start();
+
+                this.vanguardProcess.WaitForExit();
+            }
+            catch (DllNotFoundException ex)
+            {
+                ConsoleOutput.Instance.Information(ex.Message);
+            }
+            catch (NullReferenceException ex)
+            {
+                ConsoleOutput.Instance.Information(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                ConsoleOutput.Instance.Information(ex.Message);
+            }
+        }
+
+        private void CodeCoverageExited(object sender, EventArgs e)
+        {
+            if (this.vanguardProcess != null)
+            {
+                if (this.vanguardProcess.HasExited && this.vanguardProcess.ExitCode != 0)
+                {
+                    EqtTrace.Error(this.vanguardProcess.StandardError.ReadToEnd());
+                }
+
+                this.vanguardProcess.Exited -= this.CodeCoverageExited;
+            }
+
+            this.coverageXmlGenerateEvent.Set();
+        }
+
+        private string GetVSInstallPath()
+        {
+            var dllLocation = Path.GetDirectoryName(typeof(CoverageLogger).GetTypeInfo().Assembly.Location);
+            dllLocation = Path.Combine(dllLocation, IntPtr.Size == 4 ? SetupInteropx86 : SetupInteropx64);
+
+            var setupInstance = ComOperations.CreateInstanceFrom(
+                                    dllLocation,
+                                    typeof(SetupConfiguration).GetTypeInfo().GUID,
+                                    typeof(ISetupConfiguration).GetTypeInfo().GUID) as ISetupConfiguration;
+
+            IEnumSetupInstances vsInstances = setupInstance?.EnumInstances();
+
+            if (vsInstances != null)
+            {
+                vsInstances.Next(1, out ISetupInstance currentInstance, out int fetched);
+                while (currentInstance != null && fetched == 1)
+                {
+                    string installRoot = Path.GetFullPath(currentInstance.GetInstallationPath());
+                    if (!string.IsNullOrEmpty(installRoot))
+                    {
+                        return installRoot;
+                    }
+
+                    vsInstances.Next(1, out currentInstance, out fetched);
+                }
+            }
+
+            return string.Empty;
+        }
+
+        #endregion
+    }
+}
---
+++ src/Microsoft.TestPlatform.Extensions.CoverageLogger/Setup.Configuration.Interop.cs
@@ -0,0 +1,153 @@
+﻿// -----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// -----------------------------------------------------------------------------
+#pragma warning disable SA1649 // File name must match first type name
+namespace Microsoft.VisualStudio.Setup.Interop
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// Information about an instance of a product.
+    /// </summary>
+    [Guid("B41463C3-8866-43B5-BC33-2B0676F7F42E")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+
+    public interface ISetupInstance
+    {
+        /// <summary>
+        /// Gets the instance identifier (should match the name of the parent instance directory).
+        /// </summary>
+        /// <returns>The instance identifier.</returns>
+        [return: MarshalAs(UnmanagedType.BStr)]
+        string GetInstanceId();
+
+        /// <summary>
+        /// Gets the local date and time when the installation was originally installed.
+        /// </summary>
+        /// <returns>The local date and time when the installation was originally installed.</returns>
+        System.Runtime.InteropServices.ComTypes.FILETIME GetInstallDate();
+
+        /// <summary>
+        /// Gets the unique name of the installation, often indicating the branch and other information used for telemetry.
+        /// </summary>
+        /// <returns>The unique name of the installation, often indicating the branch and other information used for telemetry.</returns>
+        [return: MarshalAs(UnmanagedType.BStr)]
+        string GetInstallationName();
+
+        /// <summary>
+        /// Gets the path to the installation root of the product.
+        /// </summary>
+        /// <returns>The path to the installation root of the product.</returns>
+        [return: MarshalAs(UnmanagedType.BStr)]
+        string GetInstallationPath();
+
+        /// <summary>
+        /// Gets the version of the product installed in this instance.
+        /// </summary>
+        /// <returns>The version of the product installed in this instance.</returns>
+        [return: MarshalAs(UnmanagedType.BStr)]
+        string GetInstallationVersion();
+
+        /// <summary>
+        /// Gets the display name (title) of the product installed in this instance.
+        /// </summary>
+        /// <param name="lcid">The LCID for the display name.</param>
+        /// <returns>The display name (title) of the product installed in this instance.</returns>
+        [return: MarshalAs(UnmanagedType.BStr)]
+        string GetDisplayName([In, MarshalAs(UnmanagedType.U4)] int lcid);
+
+        /// <summary>
+        /// Gets the description of the product installed in this instance.
+        /// </summary>
+        /// <param name="lcid">The LCID for the description.</param>
+        /// <returns>The description of the product installed in this instance.</returns>
+        [return: MarshalAs(UnmanagedType.BStr)]
+        string GetDescription([In, MarshalAs(UnmanagedType.U4)] int lcid);
+
+        /// <summary>
+        /// Resolves the optional relative path to the root path of the instance.
+        /// </summary>
+        /// <param name="relativePath">A relative path within the instance to resolve, or NULL to get the root path.</param>
+        /// <returns>The full path to the optional relative path within the instance. If the relative path is NULL, the root path will always terminate in a backslash.</returns>
+        [return: MarshalAs(UnmanagedType.BStr)]
+        string ResolvePath([In, MarshalAs(UnmanagedType.LPWStr)] string relativePath);
+    }
+
+    /// <summary>
+    /// A enumerator of installed <see cref="ISetupInstance"/> objects.
+    /// </summary>
+    [Guid("6380BCFF-41D3-4B2E-8B2E-BF8A6810C848")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IEnumSetupInstances
+    {
+        /// <summary>
+        /// Retrieves the next set of product instances in the enumeration sequence.
+        /// </summary>
+        /// <param name="celt">The number of product instances to retrieve.</param>
+        /// <param name="rgelt">A pointer to an array of <see cref="ISetupInstance"/>.</param>
+        /// <param name="pceltFetched">A pointer to the number of product instances retrieved. If celt is 1 this parameter may be NULL.</param>
+        void Next(
+            [In] int celt,
+            [Out, MarshalAs(UnmanagedType.Interface)] out ISetupInstance rgelt,
+            [Out] out int pceltFetched);
+
+        /// <summary>
+        /// Skips the next set of product instances in the enumeration sequence.
+        /// </summary>
+        /// <param name="celt">The number of product instances to skip.</param>
+        void Skip([In, MarshalAs(UnmanagedType.U4)] int celt);
+
+        /// <summary>
+        /// Resets the enumeration sequence to the beginning.
+        /// </summary>
+        void Reset();
+
+        /// <summary>
+        /// Creates a new enumeration object in the same state as the current enumeration object: the new object points to the same place in the enumeration sequence.
+        /// </summary>
+        /// <returns>A pointer to a pointer to a new <see cref="IEnumSetupInstances"/> interface. If the method fails, this parameter is undefined.</returns>
+        [return: MarshalAs(UnmanagedType.Interface)]
+        IEnumSetupInstances Clone();
+    }
+
+    /// <summary>
+    /// Gets information about product instances set up on the machine.
+    /// </summary>
+    [Guid("42843719-DB4C-46C2-8E7C-64F1816EFD5B")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface ISetupConfiguration
+    {
+        /// <summary>
+        /// Enumerates all product instances installed.
+        /// </summary>
+        /// <returns>An enumeration of installed product instances.</returns>
+        [return: MarshalAs(UnmanagedType.Interface)]
+        IEnumSetupInstances EnumInstances();
+
+        /// <summary>
+        /// Gets the instance for the current process path.
+        /// </summary>
+        /// <returns>The instance for the current process path.</returns>
+        [return: MarshalAs(UnmanagedType.Interface)]
+        ISetupInstance GetInstanceForCurrentProcess();
+
+        /// <summary>
+        /// Gets the instance for the given path.
+        /// </summary>
+        /// <param name="wzPath">Path used to determine instance</param>
+        /// <returns>The instance for the given path.</returns>
+        [return: MarshalAs(UnmanagedType.Interface)]
+        ISetupInstance GetInstanceForPath([In, MarshalAs(UnmanagedType.LPWStr)] string wzPath);
+    }
+
+    /// <summary>
+    /// CoClass that implements <see cref="ISetupConfiguration"/>.
+    /// </summary>
+    [ComImport]
+    [Guid("177F0C4A-1CD3-4DE7-A32C-71DBBB9FA36D")]
+    public class SetupConfiguration
+    {
+    }
+}
+#pragma warning restore SA1649 // File name must match first type name
