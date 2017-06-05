59e046d2e47e91ba58e8817dbc8013b8571b88ef
Arun Mahapatra <codito@users.noreply.github.com>
2017-06-05 16:46:04 +0530

    Fix buffer size for socket send/receive. (#793)
    * Fix buffer size for socket send/receive.
    
    By default, BinaryWriter and Reader use a very small bufer size, which leads to multiple
    sends through the network. Increasing the buffer speeds it up by magnitudes.
    
    * Dispose streams for server and client.
    
    * Keep buffersize to 8KB. Disable Nagle's algorithm for communication.
    
    * Fix dispose order in SocketServer/Client.
    
    * Add comments to socket throughput tests

--- src/Microsoft.TestPlatform.CommunicationUtilities/SocketClient.cs
+++ src/Microsoft.TestPlatform.CommunicationUtilities/SocketClient.cs
@@ -22,6 +22,7 @@ namespace Microsoft.VisualStudio.TestPlatform.CommunicationUtilities
         private readonly TcpClient tcpClient;
         private readonly Func<Stream, ICommunicationChannel> channelFactory;
         private ICommunicationChannel channel;
+        private Stream stream;
         private bool stopped;
 
         public SocketClient()
@@ -35,7 +36,7 @@ namespace Microsoft.VisualStudio.TestPlatform.CommunicationUtilities
             this.cancellation = new CancellationTokenSource();
             this.stopped = false;
 
-            this.tcpClient = new TcpClient();
+            this.tcpClient = new TcpClient { NoDelay = true };
             this.channelFactory = channelFactory;
         }
 
@@ -69,7 +70,8 @@ namespace Microsoft.VisualStudio.TestPlatform.CommunicationUtilities
                 throw connectAsyncTask.Exception;
             }
 
-            this.channel = this.channelFactory(this.tcpClient.GetStream());
+            this.stream = new BufferedStream(this.tcpClient.GetStream(), 8 * 1024);
+            this.channel = this.channelFactory(this.stream);
             if (this.ServerConnected != null)
             {
                 this.ServerConnected.SafeInvoke(this, new ConnectedEventArgs(this.channel), "SocketClient: ServerConnected");
@@ -91,8 +93,16 @@ namespace Microsoft.VisualStudio.TestPlatform.CommunicationUtilities
                 this.stopped = true;
 
                 // Close the client and dispose the underlying stream
+                // Depending on implementation order of dispose may be important.
+                // 1. Channel dispose -> disposes reader/writer which call a Flush on the Stream. Stream shouldn't
+                // be disposed at this time.
+                // 2. Stream's dispose may Flush the underlying base stream (if it's a BufferedStream). We should try
+                // dispose it next.
+                // 3. TcpClient's dispose will clean up the network stream and close any sockets. NetworkStream's dispose
+                // is a no-op if called a second time.
+                this.channel?.Dispose();
+                this.stream?.Dispose();
                 this.tcpClient?.Dispose();
-                this.channel.Dispose();
 
                 this.cancellation.Dispose();
 
