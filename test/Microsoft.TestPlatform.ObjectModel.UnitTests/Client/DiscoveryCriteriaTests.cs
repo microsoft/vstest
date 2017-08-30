2e5c5e0e46bde00b8623b3e5d8467297c960cfb0
Mayank Bansal <maban@microsoft.com>
2017-08-31 00:25:55 +0530

    build fix

--- test/Microsoft.TestPlatform.ObjectModel.UnitTests/Client/DiscoveryCriteriaTests.cs
+++ test/Microsoft.TestPlatform.ObjectModel.UnitTests/Client/DiscoveryCriteriaTests.cs
@@ -31,7 +31,7 @@ namespace Microsoft.TestPlatform.ObjectModel.UnitTests.Client
         [TestMethod]
         public void DiscoveryCriteriaSerializesToExpectedJson()
         {
-            var expectedJson = "{\"Sources\":[\"sampleTest.dll\"],\"AdapterSourceMap\":{\"_none_\":[\"sampleTest.dll\"]},\"FrequencyOfDiscoveredTestsEvent\":100,\"DiscoveredTestEventTimeout\":\"10675199.02:48:05.4775807\",\"RunSettings\":\"<RunConfiguration></RunConfiguration>\"}";
+            var expectedJson = "{\"Package\":null,\"AdapterSourceMap\":{\"_none_\":[\"sampleTest.dll\"]},\"FrequencyOfDiscoveredTestsEvent\":100,\"DiscoveredTestEventTimeout\":\"10675199.02:48:05.4775807\",\"RunSettings\":\"<RunConfiguration></RunConfiguration>\"}";
 
             var json = JsonConvert.SerializeObject(this.discoveryCriteria, Settings);
 
