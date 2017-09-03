0367c6b6a0cad6be65ffedcc0713f0e19f2f2afc
Sarabjot Singh <sasin@microsoft.com>
2017-09-03 19:50:27 +0530

    Use vsix extensions argument processor and implementation.

---
+++ src/Microsoft.TestPlatform.Common/Interfaces/IVSExtensionManager.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TestPlatform.Common.Interfaces
+{
+    using System.Collections.Generic;
+
+    public interface IVSExtensionManager
+    {
+        /// <summary>
+        /// Initialize the manager
+        /// </summary>
+        void Initialize();
+
+        /// <summary>
+        /// Get the unit test extensions installed via vsix.
+        /// </summary>
+        IEnumerable<string> GetUnitTestExtensions();
+    }
+}
