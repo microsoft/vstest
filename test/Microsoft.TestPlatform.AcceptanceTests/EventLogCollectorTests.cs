4f58475d216695e79da715262bfa4a0b3917b8f9
Harsh Jain <harjain@microsoft.com>
2017-09-13 12:20:21 +0530

    Fix for issue https://github.com/Microsoft/vstest/issues/1076

--- test/Microsoft.TestPlatform.AcceptanceTests/EventLogCollectorTests.cs
+++ test/Microsoft.TestPlatform.AcceptanceTests/EventLogCollectorTests.cs
@@ -21,7 +21,7 @@ namespace Microsoft.TestPlatform.AcceptanceTests
 
         [CustomDataTestMethod]
         [NETFullTargetFramework]
-        public void EventLogDataCollectorShoudCreateLogFile(RunnnerInfo runnnerInfo)
+        public void EventLogDataCollectorShoudCreateLogFileHavingEvents(RunnnerInfo runnnerInfo)
         {
             SetTestEnvironment(this.testEnvironment, runnnerInfo);
             var assemblyPaths = this.testEnvironment.GetTestAsset("EventLogUnitTestProject.dll");
@@ -34,6 +34,24 @@ namespace Microsoft.TestPlatform.AcceptanceTests
 
             this.ValidateSummaryStatus(3, 0, 0);
             this.VaildateDataCollectorOutput();
+        }
+
+        [CustomDataTestMethod]
+        [NETFullTargetFramework]
+        public void EventLogDataCollectorShoudCreateLogFile(RunnnerInfo runnnerInfo)
+        {
+            SetTestEnvironment(this.testEnvironment, runnnerInfo);
+            var assemblyPaths = this.testEnvironment.GetTestAsset("SimpleTestProject.dll");
+
+            string runSettings = this.GetRunsettingsFilePath();
+            var arguments = PrepareArguments(assemblyPaths, this.GetTestAdapterPath(), runSettings, this.FrameworkArgValue);
+            arguments = string.Concat(arguments, $" /ResultsDirectory:{resultsDir}");
+
+            this.InvokeVsTest(arguments);
+
+            this.ValidateSummaryStatus(1, 1, 1);
+            this.StdErrorDoesNotContains("Data collector 'Event Log' message: Data collector 'Event Log' threw an exception during type loading, construction, or initialization:");
+            this.StdErrorDoesNotContains("Parameter name:");
         }
 
         private string GetRunsettingsFilePath()
@@ -62,6 +80,32 @@ namespace Microsoft.TestPlatform.AcceptanceTests
             return runsettingsPath;
         }
 
+        private string GetRunsettingsFilePathWithCustomSource()
+        {
+            var runsettingsPath = Path.Combine(
+                Path.GetTempPath(),
+                "test_" + Guid.NewGuid() + ".runsettings");
+
+            string runSettingsXml = @"<?xml version=""1.0"" encoding=""utf-8""?> 
+    <RunSettings>     
+      <RunConfiguration> 
+        <MaxCpuCount>0</MaxCpuCount>       
+        <TargetPlatform> x64 </TargetPlatform>     
+        <TargetFrameworkVersion> Framework45 </TargetFrameworkVersion> 
+      </RunConfiguration>
+      <DataCollectionRunSettings>  
+        <DataCollectors>  
+            <DataCollector friendlyName=""Event Log"" >
+                <Configuration><Setting name = ""EventLogs"" value = ""Application,System"" /><Setting name=""EntryTypes"" value=""Error,Warning"" /><Setting name=""EventSources"" value=""CustomEventSource"" /></Configuration>
+            </DataCollector>
+        </DataCollectors>
+      </DataCollectionRunSettings>
+    </RunSettings> ";
+
+            File.WriteAllText(runsettingsPath, runSettingsXml);
+            return runsettingsPath;
+        }
+
         private void VaildateDataCollectorOutput()
         {
             // Verify attachments
@@ -79,11 +123,11 @@ namespace Microsoft.TestPlatform.AcceptanceTests
             var fileContent3 = File.ReadAllText(resultFiles[2]);
             var fileContent4 = File.ReadAllText(resultFiles[3]);
 
-            Assert.IsTrue(fileContent1.Contains("10") && fileContent1.Contains("11") && fileContent1.Contains("12"));
-            Assert.IsTrue(fileContent2.Contains("20") && fileContent2.Contains("21") && fileContent2.Contains("22") && fileContent2.Contains("23"));
-            Assert.IsTrue(fileContent3.Contains("30") && fileContent3.Contains("31") && fileContent3.Contains("32"));
+            Assert.IsTrue(fileContent1.Contains("110") && fileContent1.Contains("111") && fileContent1.Contains("112"));
+            Assert.IsTrue(fileContent2.Contains("20") && fileContent2.Contains("221") && fileContent2.Contains("222") && fileContent2.Contains("223"));
+            Assert.IsTrue(fileContent3.Contains("330") && fileContent3.Contains("331") && fileContent3.Contains("332"));
 
-            Assert.IsTrue(fileContent4.Contains("10") && fileContent4.Contains("11") && fileContent4.Contains("12") && fileContent4.Contains("20") && fileContent4.Contains("21") && fileContent4.Contains("22") && fileContent4.Contains("23") && fileContent3.Contains("30") && fileContent4.Contains("31") && fileContent4.Contains("32"));
+            Assert.IsTrue(fileContent4.Contains("110") && fileContent4.Contains("111") && fileContent4.Contains("112") && fileContent4.Contains("220") && fileContent4.Contains("221") && fileContent4.Contains("222") && fileContent4.Contains("23") && fileContent3.Contains("330") && fileContent4.Contains("331") && fileContent4.Contains("332"));
         }
     }
 }
