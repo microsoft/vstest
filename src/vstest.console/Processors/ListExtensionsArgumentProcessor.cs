4430d03965a9039f108e990a2cf472bc3ff0c2f4
Arun Mahapatra <codito@users.noreply.github.com>
2017-09-15 10:11:30 +0530

    Remove unused NullMessageLogger

--- src/vstest.console/Processors/ListExtensionsArgumentProcessor.cs
+++ src/vstest.console/Processors/ListExtensionsArgumentProcessor.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 namespace Microsoft.VisualStudio.TestPlatform.CommandLine.Processors
@@ -230,13 +230,6 @@ namespace Microsoft.VisualStudio.TestPlatform.CommandLine.Processors
             }
 
             return ArgumentProcessorResult.Success;
-        }
-
-        private class NullMessageLogger : IMessageLogger
-        {
-            public void SendMessage(TestMessageLevel testMessageLevel, string message)
-            {
-            }
         }
     }
     #endregion
