9ffbebcd0a718a34e35302b67484e736b4e93af3
Harsh Jain <harjain@microsoft.com>
2017-10-05 21:37:11 +0530

    Added lock.

--- src/DataCollectors/Microsoft.TestPlatform.Extensions.EventLogCollector/EventLogSessionContext.cs
+++ src/DataCollectors/Microsoft.TestPlatform.Extensions.EventLogCollector/EventLogSessionContext.cs
@@ -45,7 +45,10 @@ namespace Microsoft.TestPlatform.Extensions.EventLogCollector
             {
                 kvp.Value.OnEventLogEntryWritten(kvp.Value.EventLog, null);
 
-                this.EventLogContainerEndIndexMap.Add(kvp.Key, kvp.Value.EventLogEntries.Count - 1);
+                lock (kvp.Value.EventLogEntries)
+                {
+                    this.EventLogContainerEndIndexMap.Add(kvp.Key, kvp.Value.EventLogEntries.Count - 1);
+                }
             }
         }
 
@@ -58,7 +61,10 @@ namespace Microsoft.TestPlatform.Extensions.EventLogCollector
 
             foreach (KeyValuePair<string, IEventLogContainer> kvp in this.eventLogContainerMap)
             {
-                this.EventLogContainerStartIndexMap.Add(kvp.Key, kvp.Value.EventLogEntries.Count);
+                lock (kvp.Value.EventLogEntries)
+                {
+                    this.EventLogContainerStartIndexMap.Add(kvp.Key, kvp.Value.EventLogEntries.Count);
+                }
             }
         }
     }
