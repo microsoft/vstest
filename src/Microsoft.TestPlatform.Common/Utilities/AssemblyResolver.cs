9cc5b2773d5a0720b8e7d465749c8ba346d5a901
Shyam Gupta <shgu@microsoft.com>
2017-10-25 23:36:49 -0700

    PR comments

--- src/Microsoft.TestPlatform.Common/Utilities/AssemblyResolver.cs
+++ src/Microsoft.TestPlatform.Common/Utilities/AssemblyResolver.cs
@@ -123,6 +123,7 @@ namespace Microsoft.VisualStudio.TestPlatform.Common.Utilities
                         EqtTrace.Info("AssemblyResolver: {0}: Failed to create assemblyName. Reason:{1} ", args.Name, ex);
                     }
 
+                    this.resolvedAssemblies[args.Name] = null;
                     return null;
                 }
 
