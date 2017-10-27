347c59f73943921786bf9d5a7e42d2cf011ddd15
mayankbansal018 <maban@microsoft.com>
2017-10-27 15:45:46 +0530

    Loading native dll's correctly for UWP release mode (#1234)
    * Loading native dll's correctly for UWP release mode
    
    * nit's
    
    * decouple process bitness with OS Architecture

--- src/Microsoft.TestPlatform.PlatformAbstractions/common/System/ProcessHelper.cs
+++ src/Microsoft.TestPlatform.PlatformAbstractions/common/System/ProcessHelper.cs
@@ -16,6 +16,8 @@ namespace Microsoft.VisualStudio.TestPlatform.PlatformAbstractions
     /// </summary>
     public partial class ProcessHelper : IProcessHelper
     {
+        private static readonly string ARM = "arm";
+
         /// <inheritdoc/>
         public object LaunchProcess(string processPath, string arguments, string workingDirectory, IDictionary<string, string> envVariables, Action<object, string> errorCallback, Action<object> exitCallBack)
         {
@@ -161,6 +163,24 @@ namespace Microsoft.VisualStudio.TestPlatform.PlatformAbstractions
         {
             var proc = process as Process;
             return proc?.Id ?? -1;
+        }
+
+        /// <inheritdoc/>
+        public PlatformArchitecture GetCurrentProcessArchitecture()
+        {
+            if (IntPtr.Size == 8)
+            {
+                return PlatformArchitecture.X64;
+            }
+
+            return PlatformArchitecture.X86;
+        }
+
+        /// <inheritdoc/>
+        public string GetNativeDllDirectory()
+        {
+            var isArm = Environment.GetEnvironmentVariable("PROCESSOR_ARCHITECTURE").Contains("ARM");
+            return Path.Combine(this.GetCurrentProcessLocation(), this.GetCurrentProcessArchitecture().ToString(), isArm ? ARM : string.Empty);
         }
     }
 }
