94109410abef3bdc32e60210139cb9e3f2278266
Harsh Jain <harjain@microsoft.com>
2017-11-07 21:14:45 +0530

    Formatting changes

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -361,7 +361,7 @@ function Publish-Package
 
     # Copy IntelliTrace components.
     $intellitraceSourceDirectory = Join-Path $env:TP_PACKAGES_DIR "Microsoft.Internal.Intellitrace\15.5.0-preview-20171102-03\tools"
-    $intellitraceTargetDirectory = Join-Path $env:TP_OUT_DIR "$TPB_Configuration\Intellitrace"    
+    $intellitraceTargetDirectory = Join-Path $env:TP_OUT_DIR "$TPB_Configuration\Intellitrace"
 
     if (-not (Test-Path $intellitraceTargetDirectory)) {
         New-Item $intellitraceTargetDirectory -Type Directory -Force | Out-Null
