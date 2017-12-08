19365178456d85159ad5435b4b60f1ef90338df7
Mayank Bansal <mbansal018@outlook.com>
2017-12-08 11:20:58 +0530

    Fix PS Script

--- scripts/verify-sign.ps1
+++ scripts/verify-sign.ps1
@@ -49,7 +49,8 @@ function Verify-Signature
                     if ($signature.SignerCertificate.Thumbprint -eq "98ED99A67886D020C564923B7DF25E9AC019DF26") {
                         Write-Log "Valid (Prod Signed): $($_.FullName)."
                     }
-                    else if ($signature.SignerCertificate.Thumbprint -eq "49D59D86505D82942A076388693F4FB7B21254EE") {
+                    # For some dlls e.g. "Microsoft.DiaSymReader.dll", sign certificate is different signature. Skip such binaries.
+                    elseif ($signature.SignerCertificate.Thumbprint -eq "49D59D86505D82942A076388693F4FB7B21254EE") {
                         Write-Log "Valid (Prod Signed): $($_.FullName)."
                     }
                     else {
