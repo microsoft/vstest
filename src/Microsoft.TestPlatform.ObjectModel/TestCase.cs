5b95e63093fdf7c849df8261fd4f2a34c8b9b6d0
Mayank Bansal <maban@microsoft.com>
2017-12-21 18:10:33 +0530

    Remove extra code from platform abstraction

--- src/Microsoft.TestPlatform.ObjectModel/TestCase.cs
+++ src/Microsoft.TestPlatform.ObjectModel/TestCase.cs
@@ -223,13 +223,21 @@ namespace Microsoft.VisualStudio.TestPlatform.ObjectModel
             // For example in the database adapter case this is not a file path.
             string source = this.Source;
 
-            // As discussed with team, we found no scenario for netcore, & fullclr where the Source is not present where the ID is generated,
-            // which means we would always use FileName to generated ID. In cases where somehow Source Path contained garbage character the API Path.GetFileName()
-            // In such cases we are simply returning original input.
+            // As discussed with team, we found no scenario for netcore, & fullclr where the Source is not present where ID is generated,
+            // which means we would always use FileName to generate ID. In cases where somehow Source Path contained garbage character the API Path.GetFileName()
+            // we are simply returning original input.
             // For UWP where source during discovery, & during execution can be on different machine, in such case we should always use Path.GetFileName()
-            PlatformPath.TryGetFileName(source, out string fileName);
+            try
+            {
+                // If source name is malformed, GetFileName API will throw exception, so use same input malformed string to generate ID
+                source = Path.GetFileName(source);
+            }
+            catch
+            {
+                // do nothing
+            }
 
-            string testcaseFullName = this.ExecutorUri + fileName + this.FullyQualifiedName;
+            string testcaseFullName = this.ExecutorUri + source + this.FullyQualifiedName;
             return EqtHash.GuidFromString(testcaseFullName);
         }
 
