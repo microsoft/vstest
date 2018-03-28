5e62e822dc99cc20b718b881806be62aad214692
siddhap <siddhap@microsoft.com>
2018-03-28 15:03:33 +0530

    PR comment

--- src/testhost.x86/AppDomainEngineInvoker.cs
+++ src/testhost.x86/AppDomainEngineInvoker.cs
@@ -52,12 +52,12 @@ namespace Microsoft.VisualStudio.TestPlatform.TestHost
             {
                 try
                 {
-                    if(appDomain != null)
-                    {
+                    //if(appDomain != null)
+                    //{
                         // Donot unload appdomain as there are lot is issues reported against appdomain unload
                         // any ways the process is going to die off.
                         // AppDomain.Unload(appDomain);
-                    }
+                    //}
 
                     if (!string.IsNullOrWhiteSpace(this.mergedTempConfigFile) && File.Exists(mergedTempConfigFile))
                     {
