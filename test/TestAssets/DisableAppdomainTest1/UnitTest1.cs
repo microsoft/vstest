e59dc688197e66dc0261331db61ba08ad54183fb
Siddhartha Pandey <siddhartha.pandey@microsoft.com>
2018-03-28 23:41:30 +0530

    1) in case when appdomain is created by testhost (when disableappdoma… (#1515)
    * 1) in case when appdomain is created by testhost (when disableappdomain is set to true) we also try to unload that domain. we have multiple number of issues related to appdomain unload like crashes and hangs.
    2) Will publishan RFC for testadapter writers and customer on how to workaround issues cropping up with appdomains
    30 added test which check if config file get merged in case appdomain is disabled.
    
    * fixed build break and added one more test scenario
    
    * PR comment
    
    * pr comment

---
+++ test/TestAssets/DisableAppdomainTest1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // make sure the last test was run in different process.
+            var envvariable = Environment.GetEnvironmentVariable("{60BBC8A3-3AEB-40C4-AAD5-F6DA6305C6C7}");
+            Assert.IsTrue(string.IsNullOrEmpty(envvariable));
+
+            // make sure the appdomain is the one created by testhost
+            var config = File.ReadAllText(AppDomain.CurrentDomain.SetupInformation.ConfigurationFile);
+            Assert.AreEqual("TestHostAppDomain", AppDomain.CurrentDomain.FriendlyName);
+
+            // make sure config file is honoured
+            Assert.IsTrue(config.Contains("Assembly1"));
+
+            // set env variable so that next test can assert if this is not carried forward
+            Environment.SetEnvironmentVariable("{60BBC8A3-3AEB-40C4-AAD5-F6DA6305C6C7}", "{60BBC8A3-3AEB-40C4-AAD5-F6DA6305C6C7}");
+        }
+    }
+}
