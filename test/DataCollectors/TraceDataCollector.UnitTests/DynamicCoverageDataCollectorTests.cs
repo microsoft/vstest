cc872cfc54d9d837de9792ecdcae58fbe68fbfb7
samadala <samadala@microsoft.com>
2018-04-25 18:03:29 +0530

    Add TraceDataCollector Unit tests

---
+++ test/DataCollectors/TraceDataCollector.UnitTests/DynamicCoverageDataCollectorTests.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TraceCollector.UnitTests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class DynamicCoverageDataCollectorTests
+    {
+        // [TestMethod]
+    }
+}
