6a960aa4bebc0075d95f624da54253b7340d65a4
Mayank Bansal <maban@microsoft.com>
2018-05-10 17:01:15 +0530

    Add custom attibute to exclude autogenerated code from CodeCoverage

---
+++ src/Microsoft.TestPlatform.ObjectModel/CodeCoverage/ExcludeFromCodeCoverage.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TestPlatform.ObjectModel.CodeCoverage
+{
+    using System;
+
+    public class ExcludeFromCoverage : Attribute
+    {
+    }
+}
