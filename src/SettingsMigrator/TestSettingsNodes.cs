30d840b8a9ca3bbab15d3c1cb4b3337f9481aa48
Kavipriya Adhinarayanan <kaadhina@microsoft.com>
2018-05-23 19:30:32 +0530

    Add a tool to migrate testsettings to runsettings (#1600)
    * Migrator
    
    * Adding project to sln
    
    * Refactoring
    
    * PR comments
    
    * More UTs
    
    * Fixing message
    
    * Fixing datacollector node
    
    * PR comments
    
    * Resx changes
    
    * PR comments
    
    * Comments
    
    * PR comments
    
    * Fix
    
    * Signproj
    
    * Required reference
    
    * Nuspec update
    
    * References
    
    * Fix reference
    
    * Fix references
    
    * Refactor
    
    * Fixing file count

---
+++ src/SettingsMigrator/TestSettingsNodes.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.VisualStudio.TestPlatform.SettingsMigrator
+{
+    using System.Xml;
+
+    /// <summary>
+    /// Contains the test settings nodes that need to be converted.
+    /// </summary>
+    public class TestSettingsNodes
+    {
+        public XmlNode Deployment { get; set; }
+
+        public XmlNode Script { get; set; }
+
+        public XmlNode WebSettings { get; set; }
+
+        public XmlNodeList Datacollectors { get; set; }
+
+        public XmlNode Timeout { get; set; }
+
+        public XmlNode UnitTestConfig { get; set; }
+
+        public XmlNode Hosts { get; set; }
+
+        public XmlNode Execution { get; set; }
+    }
+}
