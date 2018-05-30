8f6ab10b631a6fe8967d11edaa19e56eb93e95d6
Satya Madala <samadala@microsoft.com>
2018-05-30 17:26:03 +0530

    Display message to update Microsoft.NET.Test.Sdk (#1622)
    * Display message to update test.sdk if user try to collect cc with old test.sdk
    
    * Add more logging in Assembly resolver
    
    * Update message and refactor CreateArgument()
    
    * Add assert on size
    
    * Change Equals to AreEqual

--- src/Microsoft.TestPlatform.Build/Resources/Resources.Designer.cs
+++ src/Microsoft.TestPlatform.Build/Resources/Resources.Designer.cs
@@ -87,5 +87,14 @@ namespace Microsoft.TestPlatform.Build.Resources {
                 return ResourceManager.GetString("TestRunningSummary", resourceCulture);
             }
         }
+
+        /// <summary>
+        ///   Looks up a localized string similar to Warning: Update the Microsoft.NET.Test.Sdk package reference to version 15.8.0 or later to collect code coverage..
+        /// </summary>
+        internal static string UpdateTestSdkForCollectingCodeCoverage {
+            get {
+                return ResourceManager.GetString("UpdateTestSdkForCollectingCodeCoverage", resourceCulture);
+            }
+        }
     }
 }
