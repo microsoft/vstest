548ff82b580799cad8b7ea5c9c66581e51e03782
Sarabjot Singh <sasin@microsoft.com>
2018-12-05 15:19:12 +0530

    Adding the missing assemblyInfo files and updating the copyrights

--- src/Microsoft.TestPlatform.Build/Properties/AssemblyInfo.cs
+++ src/Microsoft.TestPlatform.Build/Properties/AssemblyInfo.cs
@@ -9,7 +9,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
 [assembly: AssemblyProduct("Microsoft.TestPlatform.Build")]
 [assembly: AssemblyTrademark("")]
 
