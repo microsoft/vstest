ee68f86f4e8d3899de5d4838863aa91277df8dca
DineshChirnanchu <Dinesh516ch@outlook.com>
2019-08-23 16:13:18 +0530

    updated new test in ArgumentProcessortests file #2071

--- test/Microsoft.TestPlatform.AcceptanceTests/ArgumentProcessorTests.cs
+++ test/Microsoft.TestPlatform.AcceptanceTests/ArgumentProcessorTests.cs
@@ -69,6 +69,5 @@ namespace Microsoft.TestPlatform.AcceptanceTests
             //Check for message which guides using help option
             this.StdErrorContains("Please use the /help option to check the list of valid arguments");
         }
-
     }
 }
