f4a68a4fe6b3225432ae66525fb141312c18e664
Maximilian Chaplin <MXCH@simcorp.com>
2020-02-26 12:09:27 +0100

    Added AssemblyInfo with ExecutionScope.ClassLevel

---
+++ test/Microsoft.TestPlatform.TestUtilities/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[assembly: Parallelize(Workers = 0, Scope = ExecutionScope.ClassLevel)]
