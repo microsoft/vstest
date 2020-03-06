88002235658091f2659247ded5e8a7a26fc799ba
Maximilian Chaplin <MXCH@simcorp.com>
2020-03-06 14:56:04 +0100

    moved brace to next line

--- test/Microsoft.TestPlatform.Utilities.UnitTests/InferRunSettingsHelperTests.cs
+++ test/Microsoft.TestPlatform.Utilities.UnitTests/InferRunSettingsHelperTests.cs
@@ -1,7 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-namespace Microsoft.TestPlatform.Utilities.UnitTests {
+namespace Microsoft.TestPlatform.Utilities.UnitTests
+{
 	using System;
 	using System.Xml;
 
