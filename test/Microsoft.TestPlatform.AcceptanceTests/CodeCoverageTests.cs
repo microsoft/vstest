ffdcd1bd9f34162b1b190f707726fc806c873449
Maximilian Chaplin <MXCH@simcorp.com>
2020-03-08 21:46:01 +0100

    tenth round of [DoNotParallelize]

--- test/Microsoft.TestPlatform.AcceptanceTests/CodeCoverageTests.cs
+++ test/Microsoft.TestPlatform.AcceptanceTests/CodeCoverageTests.cs
@@ -14,6 +14,7 @@ namespace Microsoft.TestPlatform.AcceptanceTests
 #endif
 
     [TestClass]
+    [DoNotParallelize]
     public class CodeCoverageTests : AcceptanceTestBase
     {
         private readonly string resultsDirectory;
