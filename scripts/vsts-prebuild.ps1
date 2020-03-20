b051c3434cad9b3e333a4035b81fcef4c83cac42
nohwnd <me@jakubjares.com>
2020-03-20 09:41:00 +0100

    Write value to the correct pipeline variable

--- scripts/vsts-prebuild.ps1
+++ scripts/vsts-prebuild.ps1
@@ -31,4 +31,4 @@ Write-Host "##vso[task.setvariable variable=JsonNetVersion;]$JsonNetVersion"
 
 $microsoftFakesVersion = ([xml](Get-Content $TP_ROOT_DIR\scripts\build\TestPlatform.Dependencies.props)).Project.PropertyGroup.MicrosoftFakesVersion
 $FakesPackageDir = Join-Path $TP_ROOT_DIR "packages\Microsoft.VisualStudio.TestPlatform.Fakes\$microsoftFakesVersion\lib"
-Write-Host "##vso[task.setvariable variable=JsonNetVersion;]$FakesPackageDir"
+Write-Host "##vso[task.setvariable variable=FakesPackageDir;]$FakesPackageDir"
