8468ec089570f44b0f6e907b84d88e6fa2643884
Jakub Chocholowicz <jakub.chocholowicz@gmail.com>
2020-07-02 11:24:57 +0200

    Move explanation to <remarks>

--- src/Microsoft.TestPlatform.ObjectModel/DataCollector/IDataCollectorAttachmentProcessor.cs
+++ src/Microsoft.TestPlatform.ObjectModel/DataCollector/IDataCollectorAttachmentProcessor.cs
@@ -22,8 +22,10 @@ namespace Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection
 
         /// <summary>
         /// Indicates whether attachment processor is supporting incremental processing of attachments
-        /// 
+        /// </summary>
+        /// <remarks>
         /// `SupportsIncrementalProcessing` should indicate if attachment processor is supporting incremental processing of attachments. It means that `ProcessAttachmentSetsAsync` should be [associative](https://en.wikipedia.org/wiki/Associative_property).
+        /// By default `SupportsIncrementalProcessing` should be `False`, unless processing can take longer time and it's beneficial to start the process as soon as possible.
         ///
         /// If `SupportsIncrementalProcessing` is `True` Test Platform may try to speed up whole process by reprocessing data collector attachments as soon as possible when any two test executions are done.For example let's assume we have 5 test executions which are generating 5 data collector attachments: `a1`, `a2`, `a3`, `a4` and `a5`. Test platform could perform invocations:
         /// * `var result1 = await ProcessAttachmentSetsAsync([a1, a2, a3], ...);` when first 3 executions are done
@@ -32,9 +34,7 @@ namespace Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection
         /// 
         /// If `SupportsIncrementalProcessing` is `False` then Test Platform will wait for all test executions to finish and call `ProcessAttachmentSetsAsync` only once:
         /// * `var finalResult = await ProcessAttachmentSetsAsync([a1, a2, a3, a4, a5], ...);`
-        /// 
-        /// By default `SupportsIncrementalProcessing` should be `False`, unless processing can take longer time and it's beneficial to start the process as soon as possible.
-        /// </summary>
+        /// </remarks>
         bool SupportsIncrementalProcessing { get; }
 
         /// <summary>
