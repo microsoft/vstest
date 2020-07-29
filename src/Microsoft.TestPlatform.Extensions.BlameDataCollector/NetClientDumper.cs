3616792dfb2ba94db3a9a4d07abf43df255d0533
nohwnd <me@jakubjares.com>
2020-07-29 12:36:19 +0200

    Disable dump log in normal cases

--- src/Microsoft.TestPlatform.Extensions.BlameDataCollector/NetClientDumper.cs
+++ src/Microsoft.TestPlatform.Extensions.BlameDataCollector/NetClientDumper.cs
@@ -4,13 +4,17 @@
 namespace Microsoft.TestPlatform.Extensions.BlameDataCollector
 {
     using Microsoft.Diagnostics.NETCore.Client;
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
     internal class NetClientDumper : IHangDumper
     {
         public void Dump(int processId, string outputFile, DumpTypeOption type)
         {
             var client = new DiagnosticsClient(processId);
-            client.WriteDump(type == DumpTypeOption.Full ? DumpType.Full : DumpType.Normal, outputFile, logDumpGeneration: true);
+
+            // Connecting the dump generation logging to verbose output to avoid changing the interfaces again
+            // before we test this on some big repo.
+            client.WriteDump(type == DumpTypeOption.Full ? DumpType.Full : DumpType.Normal, outputFile, logDumpGeneration: EqtTrace.IsVerboseEnabled);
         }
     }
 }
