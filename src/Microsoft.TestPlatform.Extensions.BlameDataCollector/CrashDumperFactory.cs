ef909ff093a81e96161c3e7d0fb884892836460e
nohwnd <me@jakubjares.com>
2020-08-11 11:56:32 +0200

    Use procdump even on newer fmw to dump testhost

--- src/Microsoft.TestPlatform.Extensions.BlameDataCollector/CrashDumperFactory.cs
+++ src/Microsoft.TestPlatform.Extensions.BlameDataCollector/CrashDumperFactory.cs
@@ -14,17 +14,27 @@ namespace Microsoft.TestPlatform.Extensions.BlameDataCollector
             EqtTrace.Info($"CrashDumperFactory: Creating dumper for {RuntimeInformation.OSDescription} with target framework {targetFramework}.");
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
-                if (!string.IsNullOrWhiteSpace(targetFramework) && !targetFramework.Contains("v5.0"))
-                {
-                    EqtTrace.Info($"CrashDumperFactory: This is Windows on {targetFramework} which is not net5.0 or newer, returning ProcDumpCrashDumper that uses ProcDump utility.");
-                    return new ProcDumpCrashDumper();
-                }
+                EqtTrace.Info($"CrashDumperFactory: This is Windows, returning ProcDumpCrashDumper that uses ProcDump utility.");
+                return new ProcDumpCrashDumper();
 
-                EqtTrace.Info($"CrashDumperFactory: This is Windows on {targetFramework}, returning the .NETClient dumper which uses env variables to collect crashdumps of testhost and any child process.");
+                // enable this once crashdump can trigger itself on exceptions that originate from task, then we can avoid using procdump
+                // if (!string.IsNullOrWhiteSpace(targetFramework) && !targetFramework.Contains("v5.0"))
+                // {
+                //     EqtTrace.Info($"CrashDumperFactory: This is Windows on {targetFramework} which is not net5.0 or newer, returning ProcDumpCrashDumper that uses ProcDump utility.");
+                //     return new ProcDumpCrashDumper();
+                // }
+
+                // EqtTrace.Info($"CrashDumperFactory: This is Windows on {targetFramework}, returning the .NETClient dumper which uses env variables to collect crashdumps of testhost and any child process.");
+                // return new NetClientCrashDumper();
+            }
+
+            if (!string.IsNullOrWhiteSpace(targetFramework) && !targetFramework.Contains("v5.0"))
+            {
+                EqtTrace.Info($"CrashDumperFactory: This is {RuntimeInformation.OSDescription} on {targetFramework} .NETClient dumper which uses env variables to collect crashdumps of testhost and any child process.");
                 return new NetClientCrashDumper();
             }
 
-            throw new PlatformNotSupportedException($"Unsupported operating system: {RuntimeInformation.OSDescription}");
+            throw new PlatformNotSupportedException($"Unsupported operating system: {RuntimeInformation.OSDescription}, and framework: {targetFramework}.");
         }
     }
 }
