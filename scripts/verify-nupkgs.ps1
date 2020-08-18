11936dd0d1e18169bb8b55a170924b46c60edb35
Vritant Bhardwaj <vrbhardw@microsoft.com>
2020-08-17 22:51:41 -0700

    added testrunnerharness consumption for fakes

--- scripts/verify-nupkgs.ps1
+++ scripts/verify-nupkgs.ps1
@@ -14,7 +14,7 @@ function Verify-Nuget-Packages($packageDirectory)
     Write-Log "Starting Verify-Nuget-Packages."
     $expectedNumOfFiles = @{"Microsoft.CodeCoverage" = 29;
                      "Microsoft.NET.Test.Sdk" = 13;
-                     "Microsoft.TestPlatform" = 469;
+                     "Microsoft.TestPlatform" = 477;
                      "Microsoft.TestPlatform.Build" = 19;
                      "Microsoft.TestPlatform.CLI" = 350;
                      "Microsoft.TestPlatform.Extensions.TrxLogger" = 33;
