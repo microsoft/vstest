239ef6ff4d7bf4c31633fd2b25b5199f8cf92716
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2020-10-21 14:55:07 +0200

    Added 3rdParty AuthentiCode thumbprint to the acceptlist.

--- scripts/verify-sign.ps1
+++ scripts/verify-sign.ps1
@@ -54,16 +54,20 @@ function Verify-Assemblies
                     elseif ($signature.SignerCertificate.Thumbprint -eq "5EAD300DC7E4D637948ECB0ED829A072BD152E17") {
                         Write-Log "Valid (Prod Signed): $($_.FullName)."
                     }
-		    # For some dlls e.g. "Interop.UIAutomationClient.dll", sign certificate is different signature. Skip such binaries.
+		            # For some dlls e.g. "Interop.UIAutomationClient.dll", sign certificate is different signature. Skip such binaries.
                     elseif ($signature.SignerCertificate.Thumbprint -eq "67B1757863E3EFF760EA9EBB02849AF07D3A8080") {
                         Write-Log "Valid (Prod Signed): $($_.FullName)."
                     }
-		    # For some dlls e.g. "Microsoft.VisualStudio.ArchitectureTools.PEReader.dll", sign certificate is different signature. Skip such binaries.
+		            # For some dlls e.g. "Microsoft.VisualStudio.ArchitectureTools.PEReader.dll", sign certificate is different signature. Skip such binaries.
                     elseif ($signature.SignerCertificate.Thumbprint -eq "9DC17888B5CFAD98B3CB35C1994E96227F061675") {
                         Write-Log "Valid (Prod Signed): $($_.FullName)."
                     }
-		    # For some dlls sign certificate is different signature. Skip such binaries.
+		            # For some dlls sign certificate is different signature. Skip such binaries.
                     elseif ($signature.SignerCertificate.Thumbprint -eq "62009AAABDAE749FD47D19150958329BF6FF4B34") {
+                        Write-Log "Valid (Prod Signed): $($_.FullName)."
+                    }
+                    # Microsoft 3rd Party Authenticode Signature
+                    elseif ($signature.SignerCertificate.Thumbprint -eq "899FA016DEE8E665FF2A315A1151C43FB96C430B") {
                         Write-Log "Valid (Prod Signed): $($_.FullName)."
                     }
                     else {
