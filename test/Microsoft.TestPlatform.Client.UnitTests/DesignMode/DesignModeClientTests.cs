4621f62e13265f278e2b89ec078cb7c030c20855
nohwnd <me@jakubjares.com>
2020-11-13 10:58:53 +0100

    Fix test

--- test/Microsoft.TestPlatform.Client.UnitTests/DesignMode/DesignModeClientTests.cs
+++ test/Microsoft.TestPlatform.Client.UnitTests/DesignMode/DesignModeClientTests.cs
@@ -132,7 +132,7 @@ namespace Microsoft.VisualStudio.TestPlatform.Client.UnitTests.DesignMode
         [TestMethod]
         public void DesignModeClientDuringConnectShouldHighestCommonVersionWhenReceivedVersionIsGreaterThanSupportedVersion()
         {
-            var verCheck = new Message { MessageType = MessageType.VersionCheck, Payload = 3 };
+            var verCheck = new Message { MessageType = MessageType.VersionCheck, Payload = 4 };
             var sessionEnd = new Message { MessageType = MessageType.SessionEnd };
             this.mockCommunicationManager.Setup(cm => cm.WaitForServerConnection(It.IsAny<int>())).Returns(true);
             this.mockCommunicationManager.SetupSequence(cm => cm.ReceiveMessage()).Returns(verCheck).Returns(sessionEnd);
