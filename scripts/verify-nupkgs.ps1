0d52794c643e7cb9880e9d8f5d2ee08303bce048
Codrin Poienaru <cvpoienaru@gmail.com>
2020-11-19 11:07:39 +0100

    Fixed number of files in nuget error

--- scripts/verify-nupkgs.ps1
+++ scripts/verify-nupkgs.ps1
@@ -15,7 +15,7 @@ function Verify-Nuget-Packages($packageDirectory)
     $expectedNumOfFiles = @{
                      "Microsoft.CodeCoverage" = 44;
                      "Microsoft.NET.Test.Sdk" = 18;
-                     "Microsoft.TestPlatform" = 488;
+                     "Microsoft.TestPlatform" = 487;
                      "Microsoft.TestPlatform.Build" = 19;
                      "Microsoft.TestPlatform.CLI" = 353;
                      "Microsoft.TestPlatform.Extensions.TrxLogger" = 33;
