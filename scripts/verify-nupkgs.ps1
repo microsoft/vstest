fd96af1af444b2bcc24ed0b1dbcfb7665028782b
Codrin Poienaru <cvpoienaru@gmail.com>
2020-11-19 12:15:41 +0100

    Fixed code review issues

--- scripts/verify-nupkgs.ps1
+++ scripts/verify-nupkgs.ps1
@@ -15,7 +15,7 @@ function Verify-Nuget-Packages($packageDirectory)
     $expectedNumOfFiles = @{
                      "Microsoft.CodeCoverage" = 44;
                      "Microsoft.NET.Test.Sdk" = 18;
-                     "Microsoft.TestPlatform" = 487;
+                     "Microsoft.TestPlatform" = 485;
                      "Microsoft.TestPlatform.Build" = 19;
                      "Microsoft.TestPlatform.CLI" = 353;
                      "Microsoft.TestPlatform.Extensions.TrxLogger" = 33;
