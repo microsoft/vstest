7c6ffa7071aea001e52f40790bd67de5e56ea7b6
nohwnd <me@jakubjares.com>
2020-11-24 15:14:24 +0100

    Fix loop to parse more than 1 parameter

--- scripts/test.sh
+++ scripts/test.sh
@@ -19,7 +19,8 @@ TARGET_RUNTIME="ubuntu.18.04-x64"
 FAIL_FAST=false
 VERBOSE=false
 
-while [ $# -gt 0 ]; do
+while [ $# -gt 0 ]
+do
     lowerI="$(echo ${1:-} | awk '{print tolower($0)}')"
     case $lowerI in
         -h | --help)
@@ -28,21 +29,29 @@ while [ $# -gt 0 ]; do
             ;;
         -c)
             CONFIGURATION=$2
+            shift
+            shift
             ;;
         -r)
             TARGET_RUNTIME=$2
+            shift
+            shift
             ;;
         -p)
             PROJECT_NAME_PATTERNS=$2
+            shift
+            shift
             ;;
         -verbose)
             VERBOSE=$2
+            shift
+            shift
             ;;
         *)
-            break
+            echo Unknown parameter $key
+            shift
             ;;
    esac
-   shift
 done
 
 #
