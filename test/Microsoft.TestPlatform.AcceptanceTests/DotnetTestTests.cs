c38a42f641746eed461d8c7fc995ce9233296fcf
nohwnd <me@jakubjares.com>
2021-01-13 14:37:28 +0100

    Use relative paths and correct proj

--- test/Microsoft.TestPlatform.AcceptanceTests/DotnetTestTests.cs
+++ test/Microsoft.TestPlatform.AcceptanceTests/DotnetTestTests.cs
@@ -3,13 +3,6 @@
 
 namespace Microsoft.TestPlatform.AcceptanceTests
 {
-    using System;
-    using System.IO;
-    using System.Threading;
-
-    using global::TestPlatform.TestUtilities;
-
-    using Microsoft.TestPlatform.TestUtilities;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
@@ -25,8 +18,10 @@ namespace Microsoft.TestPlatform.AcceptanceTests
 
             var projectName = "SimpleTestProject.csproj";
             var projectPath = this.GetProjectFullPath(projectName);
-            // TODO: figure out this path relative to the project
-            this.InvokeDotnetTest($@"{projectPath} --logger:""Console;Verbosity=normal"" -p:VSTestConsolePath=""C:\p\vstest\artifacts\Debug\netcoreapp2.1\vstest.console.dll""");
+
+            this.InvokeDotnetTest($@"{projectPath} --logger:""Console;Verbosity=normal""");
+            
+            // ensure our dev version is used
             this.StdOutputContains("-dev");
             this.ValidateSummaryStatus(1, 1, 1);
             this.ExitCodeEquals(1);
@@ -42,21 +37,43 @@ namespace Microsoft.TestPlatform.AcceptanceTests
             AcceptanceTestBase.SetTestEnvironment(this.testEnvironment, runnerInfo);
 
             var assemblyPath = this.BuildMultipleAssemblyPath("SimpleTestProject.dll").Trim('\"');
-            // TODO: figure out this path relative to the project
-            var vsTestConsolePath = Environment.GetEnvironmentVariable("VSTEST_CONSOLE_PATH");
-            try
-            {
-                Environment.SetEnvironmentVariable("VSTEST_CONSOLE_PATH", @"C:\p\vstest\artifacts\Debug\netcoreapp2.1\vstest.console.dll");
-                this.InvokeDotnetTest($@"{assemblyPath} --logger:""Console;Verbosity=normal""");
-            }
-            finally
-            {
-                Environment.SetEnvironmentVariable("VSTEST_CONSOLE_PATH", vsTestConsolePath);
-            }
+            this.InvokeDotnetTest($@"{assemblyPath} --logger:""Console;Verbosity=normal""");
 
-            //this.StdOutputContains("-dev");
+            // ensure our dev version is used
+            this.StdOutputContains("-dev");
             this.ValidateSummaryStatus(1, 1, 1);
             this.ExitCodeEquals(1);
+        }
+
+        [TestMethod]
+        // patched dotnet is not published on non-windows systems
+        [TestCategory("Windows-Review")]
+        [NetCoreTargetFrameworkDataSource]
+        public void PassInlineSettings(RunnerInfo runnerInfo)
+        {
+            AcceptanceTestBase.SetTestEnvironment(this.testEnvironment, runnerInfo);
+
+            var projectName = "ParametrizedTestProject.csproj";
+            var projectPath = this.GetProjectFullPath(projectName);
+            this.InvokeDotnetTest($@"{projectPath} --logger:""Console;Verbosity=normal"" -- TestRunParameters.Parameter(name =\""weburl\"", value=\""http://localhost//def\"")");
+            this.ValidateSummaryStatus(1, 0, 0);
+            this.ExitCodeEquals(0);
+        }
+
+        [TestMethod]
+        // patched dotnet is not published on non-windows systems
+        [TestCategory("Windows-Review")]
+        [NetCoreTargetFrameworkDataSource]
+        public void PassInlineSettingsToDll(RunnerInfo runnerInfo)
+        {
+
+            AcceptanceTestBase.SetTestEnvironment(this.testEnvironment, runnerInfo);
+
+            var assemblyPath = this.BuildMultipleAssemblyPath("ParametrizedTestProject.dll").Trim('\"');
+            this.InvokeDotnetTest($@"{assemblyPath} --logger:""Console;Verbosity=normal"" -- TestRunParameters.Parameter(name=\""weburl\"", value=\""http://localhost//def\"")");
+
+            this.ValidateSummaryStatus(1, 0, 0);
+            this.ExitCodeEquals(0);
         }
     }
 }
