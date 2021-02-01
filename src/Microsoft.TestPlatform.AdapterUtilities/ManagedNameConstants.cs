5091dd23993edfbf9bb3e2e0c42be46df68f9be0
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2021-02-01 15:53:46 +0100

    Added constants for hierarchical naming.
     - Added constants for hierarchical naming.
     - Reduced dependency surface of the AdapterUtilities package.

---
+++ src/Microsoft.TestPlatform.AdapterUtilities/ManagedNameConstants.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.TestPlatform.AdapterUtilities
+{
+    /// <summary>
+    /// Contants to help declare ManagedType and ManagedMethod test properties.
+    /// </summary>
+    public static class ManagedNameConstants
+    {
+        /// <summary>
+        /// Label to use on ManagedType test property.
+        /// </summary>
+        public const string ManagedTypeLabel = "ManagedType";
+
+        /// <summary>
+        /// Label to use on ManagedMethod test property.
+        /// </summary>
+        public const string ManagedMethodLabel = "ManagedMethod";
+
+        /// <summary>
+        /// Property id to use on ManagedType test property.
+        /// </summary>
+        public const string ManagedTypePropertyId = "TestCase." + ManagedTypeLabel;
+
+        /// <summary>
+        /// Property id to use on ManagedMethod test property.
+        /// </summary>
+        public const string ManagedMethodPropertyId = "TestCase." + ManagedMethodLabel;
+    }
+}
