609532fde23f9165beb0b29ff19a66243a135e2e
Jakub Chocholowicz <59966772+jakubch1@users.noreply.github.com>
2021-02-16 11:20:06 +0100

    Generating cc attachments with correct uri (#2750)
    * Generating cc attachments with correct uri
    
    * Remove constructor
    
    Co-authored-by: Jakub Chocholowicz <jachocho@microsoft.com>

--- src/Microsoft.TestPlatform.Utilities/CodeCoverageDataAttachmentsHandler.cs
+++ src/Microsoft.TestPlatform.Utilities/CodeCoverageDataAttachmentsHandler.cs
@@ -59,11 +59,11 @@ namespace Microsoft.VisualStudio.TestPlatform.Utilities
                     if (!string.IsNullOrEmpty(mergedCoverageReportFilePath))
                     {
                         var resultAttachmentSet = new AttachmentSet(CodeCoverageDataCollectorUri, CoverageFriendlyName);
-                        resultAttachmentSet.Attachments.Add(new UriDataAttachment(new Uri(mergedCoverageReportFilePath), CoverageFriendlyName));
+                        resultAttachmentSet.Attachments.Add(UriDataAttachment.CreateFrom(mergedCoverageReportFilePath, CoverageFriendlyName));
 
                         foreach (var coverageOtherFilePath in coverageOtherFilePaths)
                         {
-                            resultAttachmentSet.Attachments.Add(new UriDataAttachment(new Uri(coverageOtherFilePath), string.Empty));
+                            resultAttachmentSet.Attachments.Add(UriDataAttachment.CreateFrom(coverageOtherFilePath, string.Empty));
                         }
 
                         return new Collection<AttachmentSet> { resultAttachmentSet };
