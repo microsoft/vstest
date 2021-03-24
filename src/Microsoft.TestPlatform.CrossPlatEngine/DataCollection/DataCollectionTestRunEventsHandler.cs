f889f6d7acc0c199778f83d6642f2bef0278f912
Maximilian Chaplin <MXCH@simcorp.com>
2021-03-24 13:55:08 +0100

    S2971 Drop 'Where' and move the condition into the 'FirstOrDefault'.

--- src/Microsoft.TestPlatform.CrossPlatEngine/DataCollection/DataCollectionTestRunEventsHandler.cs
+++ src/Microsoft.TestPlatform.CrossPlatEngine/DataCollection/DataCollectionTestRunEventsHandler.cs
@@ -190,7 +190,7 @@ namespace Microsoft.VisualStudio.TestPlatform.CrossPlatEngine.DataCollection
 
             foreach (var attachmentSet in newAttachments)
             {
-                var attSet = originalAttachmentSets.Where(item => Uri.Equals(item.Uri, attachmentSet.Uri)).FirstOrDefault();
+                var attSet = originalAttachmentSets.FirstOrDefault(item => Uri.Equals(item.Uri, attachmentSet.Uri));
                 if (null == attSet)
                 {
                     originalAttachmentSets.Add(attachmentSet);
