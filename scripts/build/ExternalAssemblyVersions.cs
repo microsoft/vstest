e4bf44145fa9cafe8546a602ba95eed311eb4676
Vritant Bhardwaj <vrbhardw@microsoft.com>
2021-04-07 01:40:30 -0700

    Add support for loading Testplatform.fakes in Dev 17 (#2829)

---
+++ scripts/build/ExternalAssemblyVersions.cs
@@ -0,0 +1,12 @@
+namespace Microsoft.VisualStudio.TestPlatform
+{
+    internal class ExternalAssemblyVersions
+    {
+        /// <summary>
+        /// Refers to the versions of the assemblies retrieved from the 
+        /// Microsoft.QualityTools.Testing.Fakes.TestRunnerHarness package.
+        /// The Package version can be found in "scripts\build\TestPlatform.Dependencies.props"
+        /// </summary>
+        internal const string MicrosoftFakesAssemblyVersion = "17.0.0.0";
+    }
+}
