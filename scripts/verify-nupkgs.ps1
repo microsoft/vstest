41eb9577307003c8b86f457e9d1b851457ca8b19
Sanan Yuzbashiyev <sayuzbas@microsoft.com>
2021-06-17 18:10:12 +0200

    removing IE64.Eventhelper cause I removed it from nuget package

--- scripts/verify-nupkgs.ps1
+++ scripts/verify-nupkgs.ps1
@@ -14,7 +14,7 @@ function Verify-Nuget-Packages($packageDirectory, $version)
     $expectedNumOfFiles = @{
         "Microsoft.CodeCoverage" = 59;
         "Microsoft.NET.Test.Sdk" = 27;
-        "Microsoft.TestPlatform" = 493;
+        "Microsoft.TestPlatform" = 492;
         "Microsoft.TestPlatform.Build" = 21;
         "Microsoft.TestPlatform.CLI" = 367;
         "Microsoft.TestPlatform.Extensions.TrxLogger" = 35;
