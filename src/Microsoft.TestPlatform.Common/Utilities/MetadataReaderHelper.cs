d72d1efea13f7f2b8985bdf49a8a0d7ee6659f0f
Marco Rossignoli <marco.rossignoli@gmail.com>
2021-12-02 09:56:27 +0100

    add header

--- src/Microsoft.TestPlatform.Common/Utilities/MetadataReaderHelper.cs
+++ src/Microsoft.TestPlatform.Common/Utilities/MetadataReaderHelper.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using System;
 using System.Collections.Generic;
 using System.IO;
