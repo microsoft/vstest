ddfea67cbc4ac2e3484fbb3a0fb49a6b295e2824
Marco Rossignoli <marco.rossignoli@gmail.com>
2021-12-02 09:57:42 +0100

    reorder usings

--- src/Microsoft.TestPlatform.Common/Utilities/MetadataReaderHelper.cs
+++ src/Microsoft.TestPlatform.Common/Utilities/MetadataReaderHelper.cs
@@ -1,19 +1,20 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Reflection.Metadata;
-using System.Reflection.Metadata.Ecma335;
-using System.Reflection.PortableExecutable;
-using System.Text;
-
 namespace Microsoft.VisualStudio.TestPlatform.Common.Utilities
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Reflection.Metadata;
+    using System.Reflection.Metadata.Ecma335;
+    using System.Reflection.PortableExecutable;
+    using System.Text;
+
+    using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
     internal class MetadataReaderExtensionsHelper
     {
         private static string TestPlatformExtensionVersionAttribute = "TestPlatformExtensionVersionAttribute";
