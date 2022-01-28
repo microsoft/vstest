348260df2b847454501d815856bbd25971e8c7f1
Jakub Jareš <me@jakubjares.com>
2022-01-28 02:30:33 -0800

    Apply suggestions from code review
    Co-authored-by: Amaury Levé <amaury.leve@gmail.com>

--- src/Microsoft.TestPlatform.CoreUtilities/Utilities/MulticastDelegateUtilities.cs
+++ src/Microsoft.TestPlatform.CoreUtilities/Utilities/MulticastDelegateUtilities.cs
@@ -37,18 +37,16 @@ namespace Microsoft.VisualStudio.TestPlatform.Utilities
         /// <param name="sender">Sender to use when raising the event.</param>
         /// <param name="args">Arguments to provide.</param>
         /// <param name="traceDisplayName">Name to use when tracing out errors.</param>
-        // Using [CallerMemberName] for the traceDisplayName is a possibility, but in few places we call through other
-        // methods until we reach here. And it would change the public API.
         public static void SafeInvoke(this Delegate delegates, object sender, object args, string traceDisplayName)
         {
             if (args == null)
             {
-                throw new ArgumentNullException(Resources.CannotBeNullOrEmpty, nameof(args));
+                throw new ArgumentNullException(nameof(args));
             }
 
             if (string.IsNullOrWhiteSpace(traceDisplayName))
             {
-                throw new ArgumentException(Resources.CannotBeNullOrEmpty, nameof(traceDisplayName));
+                throw new ArgumentException(nameof(traceDisplayName));
             }
 
             if (delegates != null)
@@ -57,14 +55,13 @@ namespace Microsoft.VisualStudio.TestPlatform.Utilities
                 var i = 0;
                 foreach (Delegate handler in invocationList)
                 {
-                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    var stopwatch = Stopwatch.StartNew();
 
                     try
                     {
                         handler.DynamicInvoke(sender, args);
                         if (EqtTrace.IsVerboseEnabled)
                         {
-                            stopwatch = Stopwatch.StartNew();
                             EqtTrace.Verbose("MulticastDelegateUtilities.SafeInvoke: {0}: Invoking callback {1}/{2} for {3}.{4}, took {5} ms.",
                                     traceDisplayName,
                                     ++i,
@@ -79,7 +76,7 @@ namespace Microsoft.VisualStudio.TestPlatform.Utilities
                         if (EqtTrace.IsErrorEnabled)
                         {
                             EqtTrace.Error(
-                                "MulticastDelegateUtilities.SafeInvoke: {0}: Invoking callback {1}/{2} for {3}.{4}, failed after {5} ms with: {6}",
+                                "MulticastDelegateUtilities.SafeInvoke: {0}: Invoking callback {1}/{2} for {3}.{4}, failed after {5} ms with: {6}.",
                                 ++i,
                                 invocationList.Length,
                                 handler.GetTargetName(),
