d58101f10029ff137b1d0dd1dd90f89deb900607
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2022-02-15 12:17:23 +0100

    Added playground project.

---
+++ playground/MSTest1/UnitTest1.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
