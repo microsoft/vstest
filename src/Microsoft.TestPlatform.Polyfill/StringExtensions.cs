01532fe921e1386718e3903b09f9be3cee897a41
Amaury Levé <amauryleve@microsoft.com>
2022-02-22 21:24:34 +0100

    Refactor

--- src/Microsoft.TestPlatform.Polyfill/StringExtensions.cs
+++ src/Microsoft.TestPlatform.Polyfill/StringExtensions.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#if NETFRAMEWORK || WINDOWS_UWP || (NETCOREAPP && !NETCOREAPP2_1_OR_GREATER) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
+
 namespace System;
 
-#if NETFRAMEWORK || WINDOWS_UWP || (NETCOREAPP && !NETCOREAPP2_1_OR_GREATER) || (NETSTANDARD && !NETSTANDARD2_1_OR_GREATER)
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
 /// <summary>
 /// This type acts as a polyfill to enable using latest features will keeping compatibility with old framework.
 /// Using latest features helps us to benefit from performance/readability gain and helps with enforcing standard
@@ -14,12 +18,21 @@ namespace System;
 /// - Do not add other non-polyfilling members to this type, instead create different helper.
 /// - The full class needs to be conditionally included otherwise, despite a working build, we end up with runtime missing methods.
 /// </remarks>
-internal static class PolyfillStringExtensions
+[SuppressMessage("ApiDesign", "RS0016:Add public types and members to the declared API", Justification = PublicApiSuppressionJustification)]
+[SuppressMessage("ApiDesign", "RS0037:Enable tracking of nullability of reference types in the declared API", Justification = PublicApiSuppressionJustification)]
+public static class StringExtensions
 {
+    private const string PublicApiSuppressionJustification =
+        "Ideally we would want to the type to be internal but that's causing some ambiguous resolving. " +
+        "Besides, we want to avoid declaring this API in all of the Public API files of all projects referring this shared project.";
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool Contains(this string s, char c)
         => s.Contains(c.ToString());
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool Contains(this string s, string value, StringComparison comparisonType)
         => s.IndexOf(value, comparisonType) >= 0;
 }
+
 #endif
