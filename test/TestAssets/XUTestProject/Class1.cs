ba8b1949d46749706f1dd8f9d8d6fa22dac7e941
Amaury Levé <amauryleve@microsoft.com>
2022-02-22 11:07:52 +0100

    Update test assets

--- test/TestAssets/XUTestProject/Class1.cs
+++ test/TestAssets/XUTestProject/Class1.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Xunit;
+
 #pragma warning disable IDE1006 // Naming Styles
 namespace xUnitTestProject
 #pragma warning restore IDE1006 // Naming Styles
 {
-    using Xunit;
-
     public class Class1
     {
         [Fact]
