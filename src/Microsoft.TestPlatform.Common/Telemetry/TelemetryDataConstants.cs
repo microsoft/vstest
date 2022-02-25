d97c65eb9e06ae2b98dda58c2ed18dfb165da323
Codrin Poienaru <copoiena@microsoft.com>
2022-02-25 16:24:36 +0100

    Made telemetry data constants true constants

--- src/Microsoft.TestPlatform.Common/Telemetry/TelemetryDataConstants.cs
+++ src/Microsoft.TestPlatform.Common/Telemetry/TelemetryDataConstants.cs
@@ -12,22 +12,22 @@ namespace Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
 public static class TelemetryDataConstants
 {
     // ******************** Execution ***********************
-    public static string ParallelEnabledDuringExecution = "VS.TestRun.ParallelEnabled";
+    public const string ParallelEnabledDuringExecution = "VS.TestRun.ParallelEnabled";
 
     // Total number of tests ran under one test request
-    public static string TotalTestsRun = "VS.TestRun.TotalTests";
+    public const string TotalTestsRun = "VS.TestRun.TotalTests";
 
     // Total time taken to complete one test run request
-    public static string TimeTakenInSecForRun = "VS.TestRun.TimeTakenInSec";
+    public const string TimeTakenInSecForRun = "VS.TestRun.TimeTakenInSec";
 
-    public static string TestSettingsUsed = "VS.TestRun.IsTestSettingsUsed";
+    public const string TestSettingsUsed = "VS.TestRun.IsTestSettingsUsed";
 
-    public static string DisableAppDomain = "VS.TestRun.DisableAppDomain";
+    public const string DisableAppDomain = "VS.TestRun.DisableAppDomain";
 
     // All data related to legacy settings nodes will be prefixed with this.
-    public static string LegacySettingPrefix = "VS.TestRun.LegacySettings";
+    public const string LegacySettingPrefix = "VS.TestRun.LegacySettings";
 
-    public static string DataCollectorsEnabled = "VS.TestRun.DataCollectorsEnabled";
+    public const string DataCollectorsEnabled = "VS.TestRun.DataCollectorsEnabled";
 
     internal const string InvokedDataCollectors = "VS.TestRun.InvokedDataCollectors";
 
@@ -35,33 +35,33 @@ public static class TelemetryDataConstants
 
     internal const string DataCollectorsCoreClrProfiler = "VS.TestPlatform.DataCollector.CoreClrProfiler";
 
-    public static string RunState = "VS.TestRun.RunState";
+    public const string RunState = "VS.TestRun.RunState";
 
-    public static string NumberOfSourcesSentForRun = "VS.TestRun.NumberOfSources";
+    public const string NumberOfSourcesSentForRun = "VS.TestRun.NumberOfSources";
 
-    public static string TargetDevice = "VS.TestRun.TargetDevice";
+    public const string TargetDevice = "VS.TestRun.TargetDevice";
 
-    public static string TargetFramework = "VS.TestRun.TargetFramework";
+    public const string TargetFramework = "VS.TestRun.TargetFramework";
 
-    public static string TargetPlatform = "VS.TestRun.TargetPlatform";
+    public const string TargetPlatform = "VS.TestRun.TargetPlatform";
 
-    public static string MaxCPUcount = "VS.TestRun.MaxCPUcount";
+    public const string MaxCPUcount = "VS.TestRun.MaxCPUcount";
 
-    public static string TestPlatformVersion = "VS.TestRun.TestPlatformVersion";
+    public const string TestPlatformVersion = "VS.TestRun.TestPlatformVersion";
 
-    public static string TargetOS = "VS.TestRun.TargetOS";
+    public const string TargetOS = "VS.TestRun.TargetOS";
 
-    public static string LoggerUsed = "VS.TestRun.LoggersUsed";
+    public const string LoggerUsed = "VS.TestRun.LoggersUsed";
 
-    public static string CommandLineSwitches = "VS.TestRun.CommandLineSwitches";
+    public const string CommandLineSwitches = "VS.TestRun.CommandLineSwitches";
 
     // Adapter name will get appended. eg:- VS.TestRun.TotalTestsRun.executor//cppunittestexecutor/v1
     // In case of parallel it will be sum of all tests ran by an adapter in different execution process
-    public static string TotalTestsRanByAdapter = "VS.TestRun.TotalTestsRun";
+    public const string TotalTestsRanByAdapter = "VS.TestRun.TotalTestsRun";
 
     // Adapter name will get appended. eg:- VS.TestRun.TimeTakenToRun.executor//cppunittestexecutor/v1
     // In case of parallel it will be sum of all time taken by an adapter to run tests in different execution process
-    public static string TimeTakenToRunTestsByAnAdapter = "VS.TestRun.TimeTakenToRun";
+    public const string TimeTakenToRunTestsByAnAdapter = "VS.TestRun.TimeTakenToRun";
 
     // Reports details for MSTestV1. Reports just the count when TPv1 is used.
     // Reports legacy when the legacy runner TMI / TPv0 is used.
@@ -71,74 +71,74 @@ public static class TelemetryDataConstants
     // eg:- VS.TestRun.TotalTestsRunByMSTestv1.legacy.extension.codedui - a coded ui test using TPv0 and MSTestV1
     // Counts in this metric are not subtracted from the TotalTestsRanByAdapter. This metrics just
     // provides more insight into what was actually executed.
-    public static string TotalTestsRunByMSTestv1 = "VS.TestRun.TotalTestsRunByMSTestv1";
+    public const string TotalTestsRunByMSTestv1 = "VS.TestRun.TotalTestsRunByMSTestv1";
 
     // Total number of adapter discovered on the machine.
-    public static string NumberOfAdapterDiscoveredDuringExecution = "VS.TestRun.AdaptersDiscoveredCount";
+    public const string NumberOfAdapterDiscoveredDuringExecution = "VS.TestRun.AdaptersDiscoveredCount";
 
-    public static string NumberOfAdapterUsedToRunTests = "VS.TestRun.AdaptersUsedCount";
+    public const string NumberOfAdapterUsedToRunTests = "VS.TestRun.AdaptersUsedCount";
 
     // It will be the sum of the times taken by all adapter to run tests.
     // In case of parallel it can be more than total time taken to complete run request.
-    public static string TimeTakenByAllAdaptersInSec = "VS.TestRun.TimeTakenByAllAdapters";
+    public const string TimeTakenByAllAdaptersInSec = "VS.TestRun.TimeTakenByAllAdapters";
 
     // *********************Discovery****************************
-    public static string TotalTestsDiscovered = "VS.TestDiscovery.TotalTests";
+    public const string TotalTestsDiscovered = "VS.TestDiscovery.TotalTests";
 
-    public static string ParallelEnabledDuringDiscovery = "VS.TestDiscovery.ParallelEnabled";
+    public const string ParallelEnabledDuringDiscovery = "VS.TestDiscovery.ParallelEnabled";
 
     // All the times are in sec
-    public static string TimeTakenInSecForDiscovery = "VS.TestDiscovery.TotalTimeTakenInSec";
+    public const string TimeTakenInSecForDiscovery = "VS.TestDiscovery.TotalTimeTakenInSec";
 
-    public static string TimeTakenToLoadAdaptersInSec = "VS.TestDiscovery.TimeTakenToLoadAdaptersInSec";
+    public const string TimeTakenToLoadAdaptersInSec = "VS.TestDiscovery.TimeTakenToLoadAdaptersInSec";
 
     // It will be the sum of the times taken by all adapter to discover tests.
-    public static string TimeTakenInSecByAllAdapters = "VS.TestDiscovery.TimeTakenInSecByAllAdapters";
+    public const string TimeTakenInSecByAllAdapters = "VS.TestDiscovery.TimeTakenInSecByAllAdapters";
 
     // Adapter name will get appended. eg:- VS.TestDiscovery.TimeTakenAdapter.executor//cppunittestexecutor/v1
-    public static string TimeTakenToDiscoverTestsByAnAdapter = "VS.TestDiscovery.TimeTakenAdapter";
+    public const string TimeTakenToDiscoverTestsByAnAdapter = "VS.TestDiscovery.TimeTakenAdapter";
 
     // Adapter name will get appended. eg:- VS.TestDiscovery.TotalTestsDiscovered.executor//cppunittestexecutor/v1
-    public static string TotalTestsByAdapter = "VS.TestDiscovery.TotalTestsDiscovered";
+    public const string TotalTestsByAdapter = "VS.TestDiscovery.TotalTestsDiscovered";
 
-    public static string DiscoveryState = "VS.TestDiscovery.DiscoveryState";
+    public const string DiscoveryState = "VS.TestDiscovery.DiscoveryState";
 
-    public static string NumberOfSourcesSentForDiscovery = "VS.TestDiscovery.NumberOfSources";
+    public const string NumberOfSourcesSentForDiscovery = "VS.TestDiscovery.NumberOfSources";
 
-    public static string NumberOfAdapterDiscoveredDuringDiscovery = "VS.TestDiscovery.AdaptersDiscoveredCount";
+    public const string NumberOfAdapterDiscoveredDuringDiscovery = "VS.TestDiscovery.AdaptersDiscoveredCount";
 
-    public static string NumberOfAdapterUsedToDiscoverTests = "VS.TestDiscovery.AdaptersUsedCount";
+    public const string NumberOfAdapterUsedToDiscoverTests = "VS.TestDiscovery.AdaptersUsedCount";
 
     // *********************Attachments Processing****************************
-    public static string NumberOfAttachmentsSentForProcessing = "VS.AttachmentsProcessing.InitialAttachmentsCount";
+    public const string NumberOfAttachmentsSentForProcessing = "VS.AttachmentsProcessing.InitialAttachmentsCount";
 
-    public static string NumberOfAttachmentsAfterProcessing = "VS.AttachmentsProcessing.FinalAttachmentsCount";
+    public const string NumberOfAttachmentsAfterProcessing = "VS.AttachmentsProcessing.FinalAttachmentsCount";
 
-    public static string TimeTakenInSecForAttachmentsProcessing = "VS.AttachmentsProcessing.TotalTimeTakenInSec";
+    public const string TimeTakenInSecForAttachmentsProcessing = "VS.AttachmentsProcessing.TotalTimeTakenInSec";
 
-    public static string AttachmentsProcessingState = "VS.AttachmentsProcessing.State";
+    public const string AttachmentsProcessingState = "VS.AttachmentsProcessing.State";
 
     // *********************Test Sessions****************************
-    public static string ParallelEnabledDuringStartTestSession = "VS.TestSession.ParallelEnabled";
+    public const string ParallelEnabledDuringStartTestSession = "VS.TestSession.ParallelEnabled";
 
-    public static string TestSessionId = "VS.TestSession.Id";
+    public const string TestSessionId = "VS.TestSession.Id";
 
-    public static string TestSessionSpawnedTesthostCount = "VS.TestSession.SpawnedTesthostCount";
+    public const string TestSessionSpawnedTesthostCount = "VS.TestSession.SpawnedTesthostCount";
 
-    public static string TestSessionTesthostSpawnTimeInSec = "VS.TestSession.TesthostSpawnTimeInSec";
+    public const string TestSessionTesthostSpawnTimeInSec = "VS.TestSession.TesthostSpawnTimeInSec";
 
-    public static string TestSessionState = "VS.TestSession.State";
+    public const string TestSessionState = "VS.TestSession.State";
 
-    public static string TestSessionTotalSessionTimeInSec = "VS.TestSession.TotalSessionTimeInSec";
+    public const string TestSessionTotalSessionTimeInSec = "VS.TestSession.TotalSessionTimeInSec";
 
     // **************Events Name **********************************
-    public static string TestDiscoveryCompleteEvent = "vs/testplatform/testdiscoverysession";
+    public const string TestDiscoveryCompleteEvent = "vs/testplatform/testdiscoverysession";
 
-    public static string TestExecutionCompleteEvent = "vs/testplatform/testrunsession";
+    public const string TestExecutionCompleteEvent = "vs/testplatform/testrunsession";
 
-    public static string TestAttachmentsProcessingCompleteEvent = "vs/testplatform/testattachmentsprocessingsession";
+    public const string TestAttachmentsProcessingCompleteEvent = "vs/testplatform/testattachmentsprocessingsession";
 
-    public static string StartTestSessionCompleteEvent = "vs/testplatform/starttestsession";
+    public const string StartTestSessionCompleteEvent = "vs/testplatform/starttestsession";
 
-    public static string StopTestSessionCompleteEvent = "vs/testplatform/stoptestsession";
+    public const string StopTestSessionCompleteEvent = "vs/testplatform/stoptestsession";
 }
