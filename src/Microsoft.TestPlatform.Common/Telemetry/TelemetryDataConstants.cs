302b45ba7d22bdb6d65dad6b7e2b805be921f2d8
Codrin Poienaru <copoiena@microsoft.com>
2022-03-04 10:43:47 +0100

    Switched individual constants back to public access

--- src/Microsoft.TestPlatform.Common/Telemetry/TelemetryDataConstants.cs
+++ src/Microsoft.TestPlatform.Common/Telemetry/TelemetryDataConstants.cs
@@ -11,56 +11,56 @@ namespace Microsoft.VisualStudio.TestPlatform.Common.Telemetry;
 internal static class TelemetryDataConstants
 {
     // ******************** Execution ***********************
-    internal static readonly string ParallelEnabledDuringExecution = "VS.TestRun.ParallelEnabled";
+    public static readonly string ParallelEnabledDuringExecution = "VS.TestRun.ParallelEnabled";
 
     // Total number of tests ran under one test request
-    internal static readonly string TotalTestsRun = "VS.TestRun.TotalTests";
+    public static readonly string TotalTestsRun = "VS.TestRun.TotalTests";
 
     // Total time taken to complete one test run request
-    internal static readonly string TimeTakenInSecForRun = "VS.TestRun.TimeTakenInSec";
+    public static readonly string TimeTakenInSecForRun = "VS.TestRun.TimeTakenInSec";
 
-    internal static readonly string TestSettingsUsed = "VS.TestRun.IsTestSettingsUsed";
+    public static readonly string TestSettingsUsed = "VS.TestRun.IsTestSettingsUsed";
 
-    internal static readonly string DisableAppDomain = "VS.TestRun.DisableAppDomain";
+    public static readonly string DisableAppDomain = "VS.TestRun.DisableAppDomain";
 
     // All data related to legacy settings nodes will be prefixed with this.
-    internal static readonly string LegacySettingPrefix = "VS.TestRun.LegacySettings";
+    public static readonly string LegacySettingPrefix = "VS.TestRun.LegacySettings";
 
-    internal static readonly string DataCollectorsEnabled = "VS.TestRun.DataCollectorsEnabled";
+    public static readonly string DataCollectorsEnabled = "VS.TestRun.DataCollectorsEnabled";
 
-    internal static readonly string InvokedDataCollectors = "VS.TestRun.InvokedDataCollectors";
+    public static readonly string InvokedDataCollectors = "VS.TestRun.InvokedDataCollectors";
 
-    internal static readonly string DataCollectorsCorProfiler = "VS.TestPlatform.DataCollector.CorProfiler";
+    public static readonly string DataCollectorsCorProfiler = "VS.TestPlatform.DataCollector.CorProfiler";
 
-    internal static readonly string DataCollectorsCoreClrProfiler = "VS.TestPlatform.DataCollector.CoreClrProfiler";
+    public static readonly string DataCollectorsCoreClrProfiler = "VS.TestPlatform.DataCollector.CoreClrProfiler";
 
-    internal static readonly string RunState = "VS.TestRun.RunState";
+    public static readonly string RunState = "VS.TestRun.RunState";
 
-    internal static readonly string NumberOfSourcesSentForRun = "VS.TestRun.NumberOfSources";
+    public static readonly string NumberOfSourcesSentForRun = "VS.TestRun.NumberOfSources";
 
-    internal static readonly string TargetDevice = "VS.TestRun.TargetDevice";
+    public static readonly string TargetDevice = "VS.TestRun.TargetDevice";
 
-    internal static readonly string TargetFramework = "VS.TestRun.TargetFramework";
+    public static readonly string TargetFramework = "VS.TestRun.TargetFramework";
 
-    internal static readonly string TargetPlatform = "VS.TestRun.TargetPlatform";
+    public static readonly string TargetPlatform = "VS.TestRun.TargetPlatform";
 
-    internal static readonly string MaxCPUcount = "VS.TestRun.MaxCPUcount";
+    public static readonly string MaxCPUcount = "VS.TestRun.MaxCPUcount";
 
-    internal static readonly string TestPlatformVersion = "VS.TestRun.TestPlatformVersion";
+    public static readonly string TestPlatformVersion = "VS.TestRun.TestPlatformVersion";
 
-    internal static readonly string TargetOS = "VS.TestRun.TargetOS";
+    public static readonly string TargetOS = "VS.TestRun.TargetOS";
 
-    internal static readonly string LoggerUsed = "VS.TestRun.LoggersUsed";
+    public static readonly string LoggerUsed = "VS.TestRun.LoggersUsed";
 
-    internal static readonly string CommandLineSwitches = "VS.TestRun.CommandLineSwitches";
+    public static readonly string CommandLineSwitches = "VS.TestRun.CommandLineSwitches";
 
     // Adapter name will get appended. eg:- VS.TestRun.TotalTestsRun.executor//cppunittestexecutor/v1
     // In case of parallel it will be sum of all tests ran by an adapter in different execution process
-    internal static readonly string TotalTestsRanByAdapter = "VS.TestRun.TotalTestsRun";
+    public static readonly string TotalTestsRanByAdapter = "VS.TestRun.TotalTestsRun";
 
     // Adapter name will get appended. eg:- VS.TestRun.TimeTakenToRun.executor//cppunittestexecutor/v1
     // In case of parallel it will be sum of all time taken by an adapter to run tests in different execution process
-    internal static readonly string TimeTakenToRunTestsByAnAdapter = "VS.TestRun.TimeTakenToRun";
+    public static readonly string TimeTakenToRunTestsByAnAdapter = "VS.TestRun.TimeTakenToRun";
 
     // Reports details for MSTestV1. Reports just the count when TPv1 is used.
     // Reports legacy when the legacy runner TMI / TPv0 is used.
@@ -70,74 +70,74 @@ internal static class TelemetryDataConstants
     // eg:- VS.TestRun.TotalTestsRunByMSTestv1.legacy.extension.codedui - a coded ui test using TPv0 and MSTestV1
     // Counts in this metric are not subtracted from the TotalTestsRanByAdapter. This metrics just
     // provides more insight into what was actually executed.
-    internal static readonly string TotalTestsRunByMSTestv1 = "VS.TestRun.TotalTestsRunByMSTestv1";
+    public static readonly string TotalTestsRunByMSTestv1 = "VS.TestRun.TotalTestsRunByMSTestv1";
 
     // Total number of adapter discovered on the machine.
-    internal static readonly string NumberOfAdapterDiscoveredDuringExecution = "VS.TestRun.AdaptersDiscoveredCount";
+    public static readonly string NumberOfAdapterDiscoveredDuringExecution = "VS.TestRun.AdaptersDiscoveredCount";
 
-    internal static readonly string NumberOfAdapterUsedToRunTests = "VS.TestRun.AdaptersUsedCount";
+    public static readonly string NumberOfAdapterUsedToRunTests = "VS.TestRun.AdaptersUsedCount";
 
     // It will be the sum of the times taken by all adapter to run tests.
     // In case of parallel it can be more than total time taken to complete run request.
-    internal static readonly string TimeTakenByAllAdaptersInSec = "VS.TestRun.TimeTakenByAllAdapters";
+    public static readonly string TimeTakenByAllAdaptersInSec = "VS.TestRun.TimeTakenByAllAdapters";
 
     // *********************Discovery****************************
-    internal static readonly string TotalTestsDiscovered = "VS.TestDiscovery.TotalTests";
+    public static readonly string TotalTestsDiscovered = "VS.TestDiscovery.TotalTests";
 
-    internal static readonly string ParallelEnabledDuringDiscovery = "VS.TestDiscovery.ParallelEnabled";
+    public static readonly string ParallelEnabledDuringDiscovery = "VS.TestDiscovery.ParallelEnabled";
 
     // All the times are in sec
-    internal static readonly string TimeTakenInSecForDiscovery = "VS.TestDiscovery.TotalTimeTakenInSec";
+    public static readonly string TimeTakenInSecForDiscovery = "VS.TestDiscovery.TotalTimeTakenInSec";
 
-    internal static readonly string TimeTakenToLoadAdaptersInSec = "VS.TestDiscovery.TimeTakenToLoadAdaptersInSec";
+    public static readonly string TimeTakenToLoadAdaptersInSec = "VS.TestDiscovery.TimeTakenToLoadAdaptersInSec";
 
     // It will be the sum of the times taken by all adapter to discover tests.
-    internal static readonly string TimeTakenInSecByAllAdapters = "VS.TestDiscovery.TimeTakenInSecByAllAdapters";
+    public static readonly string TimeTakenInSecByAllAdapters = "VS.TestDiscovery.TimeTakenInSecByAllAdapters";
 
     // Adapter name will get appended. eg:- VS.TestDiscovery.TimeTakenAdapter.executor//cppunittestexecutor/v1
-    internal static readonly string TimeTakenToDiscoverTestsByAnAdapter = "VS.TestDiscovery.TimeTakenAdapter";
+    public static readonly string TimeTakenToDiscoverTestsByAnAdapter = "VS.TestDiscovery.TimeTakenAdapter";
 
     // Adapter name will get appended. eg:- VS.TestDiscovery.TotalTestsDiscovered.executor//cppunittestexecutor/v1
-    internal static readonly string TotalTestsByAdapter = "VS.TestDiscovery.TotalTestsDiscovered";
+    public static readonly string TotalTestsByAdapter = "VS.TestDiscovery.TotalTestsDiscovered";
 
-    internal static readonly string DiscoveryState = "VS.TestDiscovery.DiscoveryState";
+    public static readonly string DiscoveryState = "VS.TestDiscovery.DiscoveryState";
 
-    internal static readonly string NumberOfSourcesSentForDiscovery = "VS.TestDiscovery.NumberOfSources";
+    public static readonly string NumberOfSourcesSentForDiscovery = "VS.TestDiscovery.NumberOfSources";
 
-    internal static readonly string NumberOfAdapterDiscoveredDuringDiscovery = "VS.TestDiscovery.AdaptersDiscoveredCount";
+    public static readonly string NumberOfAdapterDiscoveredDuringDiscovery = "VS.TestDiscovery.AdaptersDiscoveredCount";
 
-    internal static readonly string NumberOfAdapterUsedToDiscoverTests = "VS.TestDiscovery.AdaptersUsedCount";
+    public static readonly string NumberOfAdapterUsedToDiscoverTests = "VS.TestDiscovery.AdaptersUsedCount";
 
     // *********************Attachments Processing****************************
-    internal static readonly string NumberOfAttachmentsSentForProcessing = "VS.AttachmentsProcessing.InitialAttachmentsCount";
+    public static readonly string NumberOfAttachmentsSentForProcessing = "VS.AttachmentsProcessing.InitialAttachmentsCount";
 
-    internal static readonly string NumberOfAttachmentsAfterProcessing = "VS.AttachmentsProcessing.FinalAttachmentsCount";
+    public static readonly string NumberOfAttachmentsAfterProcessing = "VS.AttachmentsProcessing.FinalAttachmentsCount";
 
-    internal static readonly string TimeTakenInSecForAttachmentsProcessing = "VS.AttachmentsProcessing.TotalTimeTakenInSec";
+    public static readonly string TimeTakenInSecForAttachmentsProcessing = "VS.AttachmentsProcessing.TotalTimeTakenInSec";
 
-    internal static readonly string AttachmentsProcessingState = "VS.AttachmentsProcessing.State";
+    public static readonly string AttachmentsProcessingState = "VS.AttachmentsProcessing.State";
 
     // *********************Test Sessions****************************
-    internal static readonly string ParallelEnabledDuringStartTestSession = "VS.TestSession.ParallelEnabled";
+    public static readonly string ParallelEnabledDuringStartTestSession = "VS.TestSession.ParallelEnabled";
 
-    internal static readonly string TestSessionId = "VS.TestSession.Id";
+    public static readonly string TestSessionId = "VS.TestSession.Id";
 
-    internal static readonly string TestSessionSpawnedTesthostCount = "VS.TestSession.SpawnedTesthostCount";
+    public static readonly string TestSessionSpawnedTesthostCount = "VS.TestSession.SpawnedTesthostCount";
 
-    internal static readonly string TestSessionTesthostSpawnTimeInSec = "VS.TestSession.TesthostSpawnTimeInSec";
+    public static readonly string TestSessionTesthostSpawnTimeInSec = "VS.TestSession.TesthostSpawnTimeInSec";
 
-    internal static readonly string TestSessionState = "VS.TestSession.State";
+    public static readonly string TestSessionState = "VS.TestSession.State";
 
-    internal static readonly string TestSessionTotalSessionTimeInSec = "VS.TestSession.TotalSessionTimeInSec";
+    public static readonly string TestSessionTotalSessionTimeInSec = "VS.TestSession.TotalSessionTimeInSec";
 
     // **************Events Name **********************************
-    internal static readonly string TestDiscoveryCompleteEvent = "vs/testplatform/testdiscoverysession";
+    public static readonly string TestDiscoveryCompleteEvent = "vs/testplatform/testdiscoverysession";
 
-    internal static readonly string TestExecutionCompleteEvent = "vs/testplatform/testrunsession";
+    public static readonly string TestExecutionCompleteEvent = "vs/testplatform/testrunsession";
 
-    internal static readonly string TestAttachmentsProcessingCompleteEvent = "vs/testplatform/testattachmentsprocessingsession";
+    public static readonly string TestAttachmentsProcessingCompleteEvent = "vs/testplatform/testattachmentsprocessingsession";
 
-    internal static readonly string StartTestSessionCompleteEvent = "vs/testplatform/starttestsession";
+    public static readonly string StartTestSessionCompleteEvent = "vs/testplatform/starttestsession";
 
-    internal static readonly string StopTestSessionCompleteEvent = "vs/testplatform/stoptestsession";
+    public static readonly string StopTestSessionCompleteEvent = "vs/testplatform/stoptestsession";
 }
