96a9b408d3c1382f823820648baeffef8e6d3f5f
Amaury Levé <amauryleve@microsoft.com>
2022-03-07 15:36:40 +0100

    Revert change to public API

--- src/Microsoft.TestPlatform.PlatformAbstractions/uap10.0/Runtime/PlatformAssemblyExtensions.cs
+++ src/Microsoft.TestPlatform.PlatformAbstractions/uap10.0/Runtime/PlatformAssemblyExtensions.cs
@@ -1,10 +1,11 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#if WINDOWS_UWP
+
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Reflection;
-
-#if WINDOWS_UWP
 
 #nullable disable
 
@@ -20,7 +21,8 @@ public static class PlatformAssemblyExtensions
     /// </summary>
     /// <param name="assembly">Assembly</param>
     /// <returns>Returns Assembly location as per platform</returns>
-    public static string GetAssemblyLocation(this Assembly _)
+    [SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Part of the public API")]
+    public static string GetAssemblyLocation(this Assembly assembly)
     {
         // In UWP all assemblies are packages inside Appx folder, so we return location of current directory
         return Directory.GetCurrentDirectory();
