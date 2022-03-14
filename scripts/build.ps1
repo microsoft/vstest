3cd576f42c34e31a0c159e791dd65bdb50996df7
Marco Rossignoli <marco.rossignoli@gmail.com>
2022-03-14 08:49:56 +0100

    Refactor datacollector (#3461)
    Refactor datacollector

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -258,7 +258,7 @@ function Publish-Package
     # Publish vstest.console and datacollector exclusively because *.config/*.deps.json file is not getting publish when we are publishing aforementioned project through dependency.
     Write-Log "Package: Publish src\vstest.console\vstest.console.csproj"
 
-    # We build vstest.console.arm64.exe before and we ship in the same folder as win7-x64 to have the same VSIX packaging to deploy on VS.
+    # We build vstest.console.arm64.exe before building vstest.console.exe and we put it in the same folder, so they end up shipping together.
     Publish-PackageWithRuntimeInternal $vstestConsoleProject $TPB_TargetFramework451 $TPB_ARM64_Runtime false $fullCLRPackage451Dir
     Publish-PackageWithRuntimeInternal $vstestConsoleProject $TPB_TargetFramework451 $TPB_X64_Runtime false $fullCLRPackage451Dir
     Publish-PackageInternal $vstestConsoleProject $TPB_TargetFrameworkCore20 $coreCLR20PackageDir
@@ -267,7 +267,9 @@ function Publish-Package
     Publish-PackageInternal $settingsMigratorProject $TPB_TargetFramework451 $fullCLRPackage451Dir
 
     Write-Log "Package: Publish src\datacollector\datacollector.csproj"
-    Publish-PackageInternal $dataCollectorProject $TPB_TargetFramework472 $fullCLRPackage451Dir
+    # We build datacollector.arm64.exe before building datacollector.exe and we put it in the same folder, so they end up shipping together.
+    Publish-PackageWithRuntimeInternal $dataCollectorProject $TPB_TargetFramework472 $TPB_ARM64_Runtime false $fullCLRPackage451Dir
+    Publish-PackageWithRuntimeInternal $dataCollectorProject $TPB_TargetFramework472 $TPB_X64_Runtime false $fullCLRPackage451Dir
     Publish-PackageInternal $dataCollectorProject $TPB_TargetFrameworkCore20 $coreCLR20PackageDir
 
     ################################################################################
@@ -343,8 +345,9 @@ function Publish-Package
     Copy-Item $testhostFullPackageDir\* $fullDestDir -Force -Recurse
 
     # Copy over the Full CLR built datacollector package assemblies to the Core CLR package folder along with testhost
-    Publish-PackageInternal $dataCollectorProject $TPB_TargetFramework472 $fullDestDir
-
+    Publish-PackageWithRuntimeInternal $dataCollectorProject $TPB_TargetFramework472 $TPB_ARM64_Runtime false $fullDestDir
+    Publish-PackageWithRuntimeInternal $dataCollectorProject $TPB_TargetFramework472 $TPB_X64_Runtime false $fullDestDir
+    
     New-Item -ItemType directory -Path $fullCLRPackage451Dir -Force | Out-Null
     Copy-Item $testhostFullPackageDir\* $fullCLRPackage451Dir -Force -Recurse
 
