8c0cc0c62060da8c0cd8e95a6dcbcb9dcfc19860
Jakub Jareš <me@jakubjares.com>
2022-03-29 09:11:51 +0200

    Add tools and fix gitingore

---
+++ test/TestAssets/Tools/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+// This project is used to restore TestPlatform and TestPlatform.CLI tools packages
+// for testing with older versions.
+
+Console.WriteLine("Hello, World!");
