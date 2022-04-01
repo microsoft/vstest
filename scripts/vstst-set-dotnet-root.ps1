670781e40d81d4ae73cafbde8d989df673d20592
Jakub Jareš <me@jakubjares.com>
2022-04-01 10:08:15 +0200

    Remove unused script

--- scripts/vstst-set-dotnet-root.ps1
+++
@@ -1,8 +0,0 @@
-param ()
-
-$TP_ROOT_DIR = (Get-Item (Split-Path $MyInvocation.MyCommand.Path)).Parent.FullName
-Write-Host "##vso[task.setvariable variable=DOTNET_ROOT;]$TP_ROOT_DIR\tools\dotnet"
-Write-Host "##vso[task.setvariable variable=DOTNET_ROOT(x86);]$TP_ROOT_DIR\tools\dotnet_x86"
-Write-Host "##vso[task.setvariable variable=PATH;]$TP_ROOT_DIR\tools\dotnet;$env:PATH"
-
-
