3c00b089854d9c8411229de800c9e4ae22a29fb1
Marco Rossignoli <marco.rossignoli@gmail.com>
2022-04-05 09:41:37 +0200

    Bundle arm64 managed code coverage support (#3547)
    Bundle arm64 managed code coverage support

--- scripts/verify-sign.ps1
+++ scripts/verify-sign.ps1
@@ -35,7 +35,7 @@ $TPB_AssembliesPattern = @(
     "*test*.dll", "*qualitytools*.dll", "*test*.exe", "*datacollector*.dll", "*datacollector*.exe",
     "QTAgent*.exe", "Microsoft.VisualStudio*.dll", "Microsoft.TestPlatform.Build.dll", "Microsoft.DiaSymReader.dll",
     "Microsoft.IntelliTrace*.dll", "concrt140.dll", "msvcp140.dll", "vccorlib140.dll", "vcruntime140.dll", "codecoveragemessages.dll",
-    "covrun32.dll", "msdia140.dll", "covrun64.dll", "IntelliTrace.exe", "ProcessSnapshotCleanup.exe", "TDEnvCleanup.exe",
+    "covrun32.dll", "msdia140.dll", "covrun64.dll", "covrunarm64.dll", "IntelliTrace.exe", "ProcessSnapshotCleanup.exe", "TDEnvCleanup.exe",
     "CodeCoverage.exe", "Microsoft.ShDocVw.dll", "UIAComwrapper.dll", "Interop.UIAutomationClient.dll", "SettingsMigrator.exe",
     "Newtonsoft.Json.dll", "DumpMinitool*.exe"
 )
