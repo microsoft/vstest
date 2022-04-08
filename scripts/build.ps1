73d0d71cfddca87b93bbdbd1160635a956ffc1ce
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2022-04-08 10:47:01 +0200

    Do not remove packages folder in ci

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -887,10 +887,12 @@ function Create-NugetPackages {
 
     Copy-Item (Join-Path $env:TP_PACKAGE_PROJ_DIR "Icon.png") $stagingDir -Force
 
-    # Remove all locally built nuget packages before we start creating them
-    # we are leaving them in the folder after uzipping them for easier review.
-    if (Test-Path $packageOutputDir) {
-        Remove-Item $packageOutputDir -Recurse -Force -Exclude "manifest","source-build"
+    if (-not $TPB_CIBuild) {
+        # Remove all locally built nuget packages before we start creating them
+        # we are leaving them in the folder after uzipping them for easier review.
+        if (Test-Path $packageOutputDir) {
+            Remove-Item $packageOutputDir -Recurse -Force
+        }
     }
 
     if (-not (Test-Path $packageOutputDir)) {
