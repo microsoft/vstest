c34ad53eae6060968f44bb803f96838e273e5f73
Medeni Baykal <433724+Haplois@users.noreply.github.com>
2022-04-08 11:45:11 +0200

    Update build.ps1

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -887,6 +887,8 @@ function Create-NugetPackages {
 
     Copy-Item (Join-Path $env:TP_PACKAGE_PROJ_DIR "Icon.png") $stagingDir -Force
 
+    # Packages folder should not be cleared on CI.
+    # Artifacts from source-build are downloaded into this directory before the build starts, and this would remove them.
     if (-not $TPB_CIBuild) {
         # Remove all locally built nuget packages before we start creating them
         # we are leaving them in the folder after uzipping them for easier review.
