a30024ae071f4aae1793d9dd76dbee73ea81ba65
Jakub Jareš <me@jakubjares.com>
2022-04-11 18:32:12 +0200

    Change perf tests granularity

--- test/TestAssets/performance/NUnit100Passing/NunitTests.cs
+++ test/TestAssets/performance/NUnit100Passing/NunitTests.cs
@@ -1,417 +1,552 @@
 ﻿using System;
 using NUnit.Framework;
 
-namespace NUnitAdapterPerfTests
+namespace Tests
 {
-
-    public class NunitTests
+    [TestFixture]
+    public class TestClass_01
     {
-
         #region PassingTests
         [Test]
-        public void Test_01()
+        public void Test_01_01()
         {
         }
+
         [Test]
-        public void Test_02()
+        public void Test_01_02()
         {
         }
+
         [Test]
-        public void Test_03()
+        public void Test_01_03()
         {
         }
+
         [Test]
-        public void Test_04()
+        public void Test_01_04()
         {
         }
+
         [Test]
-        public void Test_05()
+        public void Test_01_05()
         {
         }
+
         [Test]
-        public void Test_06()
+        public void Test_01_06()
         {
         }
+
         [Test]
-        public void Test_07()
+        public void Test_01_07()
         {
         }
+
         [Test]
-        public void Test_08()
+        public void Test_01_08()
         {
         }
+
         [Test]
-        public void Test_09()
+        public void Test_01_09()
         {
         }
+
         [Test]
-        public void Test_10()
+        public void Test_01_10()
         {
         }
+
         [Test]
-        public void Test_11()
+        public void Test_01_11()
         {
         }
+
         [Test]
-        public void Test_12()
+        public void Test_01_12()
         {
         }
+
         [Test]
-        public void Test_13()
+        public void Test_01_13()
         {
         }
+
         [Test]
-        public void Test_14()
+        public void Test_01_14()
         {
         }
+
         [Test]
-        public void Test_15()
+        public void Test_01_15()
         {
         }
+
         [Test]
-        public void Test_16()
+        public void Test_01_16()
         {
         }
+
         [Test]
-        public void Test_17()
+        public void Test_01_17()
         {
         }
+
         [Test]
-        public void Test_18()
+        public void Test_01_18()
         {
         }
+
         [Test]
-        public void Test_19()
+        public void Test_01_19()
         {
         }
+
         [Test]
-        public void Test_20()
+        public void Test_01_20()
         {
         }
-        [Test]
-        public void Test_21()
-        {
-        }
-        [Test]
-        public void Test_22()
-        {
-        }
-        [Test]
-        public void Test_23()
-        {
-        }
-        [Test]
-        public void Test_24()
-        {
-        }
-        [Test]
-        public void Test_25()
-        {
-        }
-        [Test]
-        public void Test_26()
-        {
-        }
-        [Test]
-        public void Test_27()
-        {
-        }
-        [Test]
-        public void Test_28()
-        {
-        }
-        [Test]
-        public void Test_29()
-        {
-        }
-        [Test]
-        public void Test_30()
-        {
-        }
-        [Test]
-        public void Test_31()
-        {
-        }
-        [Test]
-        public void Test_32()
-        {
-        }
-        [Test]
-        public void Test_33()
-        {
-        }
-        [Test]
-        public void Test_34()
-        {
-        }
-        [Test]
-        public void Test_35()
-        {
-        }
-        [Test]
-        public void Test_36()
-        {
-        }
-        [Test]
-        public void Test_37()
-        {
-        }
-        [Test]
-        public void Test_38()
-        {
-        }
-        [Test]
-        public void Test_39()
-        {
-        }
-        [Test]
-        public void Test_40()
-        {
-        }
-        [Test]
-        public void Test_41()
-        {
-        }
-        [Test]
-        public void Test_42()
-        {
-        }
-        [Test]
-        public void Test_43()
-        {
-        }
-        [Test]
-        public void Test_44()
-        {
-        }
-        [Test]
-        public void Test_45()
-        {
-        }
-        [Test]
-        public void Test_46()
-        {
-        }
-        [Test]
-        public void Test_47()
-        {
-        }
-        [Test]
-        public void Test_48()
-        {
-        }
-        [Test]
-        public void Test_49()
-        {
-        }
-        [Test]
-        public void Test_50()
-        {
-        }
-        [Test]
-        public void Test_51()
-        {
-        }
-        [Test]
-        public void Test_52()
-        {
-        }
-        [Test]
-        public void Test_53()
-        {
-        }
-        [Test]
-        public void Test_54()
-        {
-        }
-        [Test]
-        public void Test_55()
-        {
-        }
-        [Test]
-        public void Test_56()
-        {
-        }
-        [Test]
-        public void Test_57()
-        {
-        }
-        [Test]
-        public void Test_58()
-        {
-        }
-        [Test]
-        public void Test_59()
-        {
-        }
-        [Test]
-        public void Test_60()
-        {
-        }
-        [Test]
-        public void Test_61()
-        {
-        }
-        [Test]
-        public void Test_62()
-        {
-        }
-        [Test]
-        public void Test_63()
-        {
-        }
-        [Test]
-        public void Test_64()
-        {
-        }
-        [Test]
-        public void Test_65()
-        {
-        }
-        [Test]
-        public void Test_66()
-        {
-        }
-        [Test]
-        public void Test_67()
-        {
-        }
-        [Test]
-        public void Test_68()
-        {
-        }
-        [Test]
-        public void Test_69()
-        {
-        }
-        [Test]
-        public void Test_70()
-        {
-        }
-        [Test]
-        public void Test_71()
-        {
-        }
-        [Test]
-        public void Test_72()
-        {
-        }
-        [Test]
-        public void Test_73()
-        {
-        }
-        [Test]
-        public void Test_74()
-        {
-        }
-        [Test]
-        public void Test_75()
-        {
-        }
-        [Test]
-        public void Test_76()
-        {
-        }
-        [Test]
-        public void Test_77()
-        {
-        }
-        [Test]
-        public void Test_78()
-        {
-        }
-        [Test]
-        public void Test_79()
-        {
-        }
-        [Test]
-        public void Test_80()
-        {
-        }
-        [Test]
-        public void Test_81()
-        {
-        }
-        [Test]
-        public void Test_82()
-        {
-        }
-        [Test]
-        public void Test_83()
-        {
-        }
-        [Test]
-        public void Test_84()
-        {
-        }
-        [Test]
-        public void Test_85()
-        {
-        }
-        [Test]
-        public void Test_86()
-        {
-        }
-        [Test]
-        public void Test_87()
-        {
-        }
-        [Test]
-        public void Test_88()
-        {
-        }
-        [Test]
-        public void Test_89()
-        {
-        }
-        [Test]
-        public void Test_90()
-        {
-        }
-        [Test]
-        public void Test_91()
-        {
-        }
-        [Test]
-        public void Test_92()
-        {
-        }
-        [Test]
-        public void Test_93()
-        {
-        }
-        [Test]
-        public void Test_94()
-        {
-        }
-        [Test]
-        public void Test_95()
-        {
-        }
-        [Test]
-        public void Test_96()
-        {
-        }
-        [Test]
-        public void Test_97()
-        {
-        }
-        [Test]
-        public void Test_98()
-        {
-        }
-        [Test]
-        public void Test_99()
-        {
-        }
-        [Test]
-        public void Test_100()
-        {
-        }
+
         #endregion
 
         #region FailingTests
         #endregion
-       
     }
+    [TestFixture]
+    public class TestClass_02
+    {
+        #region PassingTests
+        [Test]
+        public void Test_02_01()
+        {
+        }
+
+        [Test]
+        public void Test_02_02()
+        {
+        }
+
+        [Test]
+        public void Test_02_03()
+        {
+        }
+
+        [Test]
+        public void Test_02_04()
+        {
+        }
+
+        [Test]
+        public void Test_02_05()
+        {
+        }
+
+        [Test]
+        public void Test_02_06()
+        {
+        }
+
+        [Test]
+        public void Test_02_07()
+        {
+        }
+
+        [Test]
+        public void Test_02_08()
+        {
+        }
+
+        [Test]
+        public void Test_02_09()
+        {
+        }
+
+        [Test]
+        public void Test_02_10()
+        {
+        }
+
+        [Test]
+        public void Test_02_11()
+        {
+        }
+
+        [Test]
+        public void Test_02_12()
+        {
+        }
+
+        [Test]
+        public void Test_02_13()
+        {
+        }
+
+        [Test]
+        public void Test_02_14()
+        {
+        }
+
+        [Test]
+        public void Test_02_15()
+        {
+        }
+
+        [Test]
+        public void Test_02_16()
+        {
+        }
+
+        [Test]
+        public void Test_02_17()
+        {
+        }
+
+        [Test]
+        public void Test_02_18()
+        {
+        }
+
+        [Test]
+        public void Test_02_19()
+        {
+        }
+
+        [Test]
+        public void Test_02_20()
+        {
+        }
+
+        #endregion
+
+        #region FailingTests
+        #endregion
+    }
+    [TestFixture]
+    public class TestClass_03
+    {
+        #region PassingTests
+        [Test]
+        public void Test_03_01()
+        {
+        }
+
+        [Test]
+        public void Test_03_02()
+        {
+        }
+
+        [Test]
+        public void Test_03_03()
+        {
+        }
+
+        [Test]
+        public void Test_03_04()
+        {
+        }
+
+        [Test]
+        public void Test_03_05()
+        {
+        }
+
+        [Test]
+        public void Test_03_06()
+        {
+        }
+
+        [Test]
+        public void Test_03_07()
+        {
+        }
+
+        [Test]
+        public void Test_03_08()
+        {
+        }
+
+        [Test]
+        public void Test_03_09()
+        {
+        }
+
+        [Test]
+        public void Test_03_10()
+        {
+        }
+
+        [Test]
+        public void Test_03_11()
+        {
+        }
+
+        [Test]
+        public void Test_03_12()
+        {
+        }
+
+        [Test]
+        public void Test_03_13()
+        {
+        }
+
+        [Test]
+        public void Test_03_14()
+        {
+        }
+
+        [Test]
+        public void Test_03_15()
+        {
+        }
+
+        [Test]
+        public void Test_03_16()
+        {
+        }
+
+        [Test]
+        public void Test_03_17()
+        {
+        }
+
+        [Test]
+        public void Test_03_18()
+        {
+        }
+
+        [Test]
+        public void Test_03_19()
+        {
+        }
+
+        [Test]
+        public void Test_03_20()
+        {
+        }
+
+        #endregion
+
+        #region FailingTests
+        #endregion
+    }
+    [TestFixture]
+    public class TestClass_04
+    {
+        #region PassingTests
+        [Test]
+        public void Test_04_01()
+        {
+        }
+
+        [Test]
+        public void Test_04_02()
+        {
+        }
+
+        [Test]
+        public void Test_04_03()
+        {
+        }
+
+        [Test]
+        public void Test_04_04()
+        {
+        }
+
+        [Test]
+        public void Test_04_05()
+        {
+        }
+
+        [Test]
+        public void Test_04_06()
+        {
+        }
+
+        [Test]
+        public void Test_04_07()
+        {
+        }
+
+        [Test]
+        public void Test_04_08()
+        {
+        }
+
+        [Test]
+        public void Test_04_09()
+        {
+        }
+
+        [Test]
+        public void Test_04_10()
+        {
+        }
+
+        [Test]
+        public void Test_04_11()
+        {
+        }
+
+        [Test]
+        public void Test_04_12()
+        {
+        }
+
+        [Test]
+        public void Test_04_13()
+        {
+        }
+
+        [Test]
+        public void Test_04_14()
+        {
+        }
+
+        [Test]
+        public void Test_04_15()
+        {
+        }
+
+        [Test]
+        public void Test_04_16()
+        {
+        }
+
+        [Test]
+        public void Test_04_17()
+        {
+        }
+
+        [Test]
+        public void Test_04_18()
+        {
+        }
+
+        [Test]
+        public void Test_04_19()
+        {
+        }
+
+        [Test]
+        public void Test_04_20()
+        {
+        }
+
+        #endregion
+
+        #region FailingTests
+        #endregion
+    }
+    [TestFixture]
+    public class TestClass_05
+    {
+        #region PassingTests
+        [Test]
+        public void Test_05_01()
+        {
+        }
+
+        [Test]
+        public void Test_05_02()
+        {
+        }
+
+        [Test]
+        public void Test_05_03()
+        {
+        }
+
+        [Test]
+        public void Test_05_04()
+        {
+        }
+
+        [Test]
+        public void Test_05_05()
+        {
+        }
+
+        [Test]
+        public void Test_05_06()
+        {
+        }
+
+        [Test]
+        public void Test_05_07()
+        {
+        }
+
+        [Test]
+        public void Test_05_08()
+        {
+        }
+
+        [Test]
+        public void Test_05_09()
+        {
+        }
+
+        [Test]
+        public void Test_05_10()
+        {
+        }
+
+        [Test]
+        public void Test_05_11()
+        {
+        }
+
+        [Test]
+        public void Test_05_12()
+        {
+        }
+
+        [Test]
+        public void Test_05_13()
+        {
+        }
+
+        [Test]
+        public void Test_05_14()
+        {
+        }
+
+        [Test]
+        public void Test_05_15()
+        {
+        }
+
+        [Test]
+        public void Test_05_16()
+        {
+        }
+
+        [Test]
+        public void Test_05_17()
+        {
+        }
+
+        [Test]
+        public void Test_05_18()
+        {
+        }
+
+        [Test]
+        public void Test_05_19()
+        {
+        }
+
+        [Test]
+        public void Test_05_20()
+        {
+        }
+
+        #endregion
+
+        #region FailingTests
+        #endregion
+    }
+ 
 }
