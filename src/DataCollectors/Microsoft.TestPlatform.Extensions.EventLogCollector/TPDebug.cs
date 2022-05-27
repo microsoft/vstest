7ae86679ded201b6828fa8013e63718b87b68a5e
Amaury Levé <amauryleve@microsoft.com>
2022-05-27 14:51:35 +0200

    Enable nullable for EventLogCollector (#3674)

---
+++ src/DataCollectors/Microsoft.TestPlatform.Extensions.EventLogCollector/TPDebug.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics;
+
+using SystemDebug = System.Diagnostics.Debug;
+
+namespace Microsoft.TestPlatform;
+
+internal static class TPDebug
+{
+    /// <inheritdoc cref="SystemDebug.Assert(bool)"/>
+    [Conditional("DEBUG")]
+    public static void Assert([DoesNotReturnIf(false)] bool b)
+        => SystemDebug.Assert(b);
+
+    /// <inheritdoc cref="SystemDebug.Assert(bool, string)"/>
+    [Conditional("DEBUG")]
+    public static void Assert([DoesNotReturnIf(false)] bool b, string message)
+        => SystemDebug.Assert(b, message);
+}
