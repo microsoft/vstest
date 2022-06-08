db760b3601edf2ed1550d4978cd7342d383b3786
Amaury Levé <amauryleve@microsoft.com>
2022-06-08 11:05:25 +0200

    Add integration test

---
+++ test/Microsoft.TestPlatform.AcceptanceTests/DotnetArchitectureTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#if !NETFRAMEWORK
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.TestPlatform.AcceptanceTests;
+
+[TestClass]
+public class DotnetArchitectureTests : AcceptanceTestBase
+{
+    [TestMethod]
+    [NetCoreTargetFrameworkDataSource(useDesktopRunner: false)]
+    public void DotnetTestProjectLaunching32BitsProcess(RunnerInfo runnerInfo)
+    {
+        SetTestEnvironment(_testEnvironment, runnerInfo);
+
+        var projectPath = GetTestDllForFramework("Net6Launches32BitsProcess.dll", "net6.0");
+        InvokeDotnetTest(projectPath);
+
+        ExitCodeEquals(0);
+    }
+}
+
+#endif
---
+++ test/TestAssets/Net6Launches32BitsProcess/Test32Bit.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTest;
+
+[TestClass]
+public class Test32Bit
+{
+    [TestMethod]
+    public void TheTest()
+    {
+        var process = new Process();
+        process.StartInfo = new ProcessStartInfo
+        {
+            FileName = "TestProcess32.exe",
+            RedirectStandardError = true,
+            RedirectStandardOutput = true
+        };
+
+        process.Start();
+        var stderr = process.StandardError.ReadToEnd();
+        var stdout = process.StandardOutput.ReadToEnd();
+
+        Console.WriteLine($"32bit stdout: {stdout}");
+        Console.WriteLine($"32bit err: {stderr}");
+
+        Assert.IsTrue(string.IsNullOrEmpty(stderr));
+    }
+}
