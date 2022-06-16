fd64765198b3b9309361a203f40db4660f51e93c
Marco Rossignoli <mrossignol@microsoft.com>
2022-06-16 14:08:42 +0200

    add test

--- test/Microsoft.TestPlatform.AcceptanceTests/DotnetTestTests.cs
+++ test/Microsoft.TestPlatform.AcceptanceTests/DotnetTestTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.IO;
+
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.TestPlatform.AcceptanceTests;
@@ -70,5 +72,22 @@ public class DotnetTestTests : AcceptanceTestBase
 
         ValidateSummaryStatus(1, 0, 0);
         ExitCodeEquals(0);
+    }
+
+    [TestMethod]
+    // patched dotnet is not published on non-windows systems
+    [TestCategory("Windows-Review")]
+    [NetCoreTargetFrameworkDataSource(useDesktopRunner: false)]
+    public void RunDotnetTestWithNativeDll(RunnerInfo runnerInfo)
+    {
+        SetTestEnvironment(_testEnvironment, runnerInfo);
+
+        string assemblyRelativePath = @"microsoft.testplatform.testasset.nativecpp\2.0.0\contentFiles\any\any\x64\Microsoft.TestPlatform.TestAsset.NativeCPP.dll";
+        var assemblyAbsolutePath = Path.Combine(_testEnvironment.PackageDirectory, assemblyRelativePath);
+
+        InvokeDotnetTest($@"{assemblyAbsolutePath} --logger:""Console;Verbosity=normal"" --diag:log.txt");
+
+        ValidateSummaryStatus(1, 1, 0);
+        ExitCodeEquals(1);
     }
 }
