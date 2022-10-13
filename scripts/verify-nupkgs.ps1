6defc0d4968a73551fac9a947693d2e9a556bb97
Jakub Chocholowicz <59966772+jakubch1@users.noreply.github.com>
2022-10-13 17:41:45 +0200

    Add Mono.Cecil.Rocks (#4071)
    Co-authored-by: Jakub Chocholowicz <jachocho@microsoft.com>

--- scripts/verify-nupkgs.ps1
+++ scripts/verify-nupkgs.ps1
@@ -12,9 +12,9 @@ function Verify-Nuget-Packages($packageDirectory, $version)
 {
     Write-Log "Starting Verify-Nuget-Packages."
     $expectedNumOfFiles = @{
-        "Microsoft.CodeCoverage" = 57;
+        "Microsoft.CodeCoverage" = 58;
         "Microsoft.NET.Test.Sdk" = 16;
-        "Microsoft.TestPlatform" = 602;
+        "Microsoft.TestPlatform" = 603;
         "Microsoft.TestPlatform.Build" = 21;
         "Microsoft.TestPlatform.CLI" = 479;
         "Microsoft.TestPlatform.Extensions.TrxLogger" = 35;
