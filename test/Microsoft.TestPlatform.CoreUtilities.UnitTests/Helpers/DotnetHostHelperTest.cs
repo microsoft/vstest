a1bf5a064d4c40c042639f0e64df0bacc898fa23
Marco Rossignoli <mrossignol@microsoft.com>
2022-12-16 15:51:16 +0100

    cleanup

--- test/Microsoft.TestPlatform.CoreUtilities.UnitTests/Helpers/DotnetHostHelperTest.cs
+++ test/Microsoft.TestPlatform.CoreUtilities.UnitTests/Helpers/DotnetHostHelperTest.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 using Microsoft.VisualStudio.TestPlatform.CoreUtilities.Helpers;
