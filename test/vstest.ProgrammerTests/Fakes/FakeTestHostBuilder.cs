f33dd41c926908ab89803a5b20effef32b8789b9
stan-sz <37585349+stan-sz@users.noreply.github.com>
2022-12-28 08:47:53 +0000

    A batch of fixes or suppressions

--- test/vstest.ProgrammerTests/Fakes/FakeTestHostBuilder.cs
+++ test/vstest.ProgrammerTests/Fakes/FakeTestHostBuilder.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Interfaces;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace vstest.ProgrammerTests.Fakes;
