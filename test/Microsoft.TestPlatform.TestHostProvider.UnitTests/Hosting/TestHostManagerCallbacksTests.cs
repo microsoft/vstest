d804d248c253b5994ae02760db2898132cdb3cdb
Marco Rossignoli <marco.rossignoli@gmail.com>
2023-02-07 22:48:55 +0100

    Catch unhandled exception and avoid crash on test host exit (#4291)
    * Do not crash if process invalid handle
    
    * revert global.json
    
    * fix build
    
    * Update src/Microsoft.TestPlatform.TestHostProvider/Hosting/TestHostManagerCallbacks.cs
    
    Co-authored-by: Amaury Levé <amaury.leve@gmail.com>
    
    * Update test/Microsoft.TestPlatform.TestHostProvider.UnitTests/Hosting/TestHostManagerCallbacksTests.cs
    
    Co-authored-by: Amaury Levé <amaury.leve@gmail.com>
    
    * Update test/Microsoft.TestPlatform.TestHostProvider.UnitTests/Hosting/TestHostManagerCallbacksTests.cs
    
    Co-authored-by: Amaury Levé <amaury.leve@gmail.com>
    
    ---------
    
    Co-authored-by: Marco Rossignoli <mrossignol@microsoft.com>
    Co-authored-by: Amaury Levé <amaury.leve@gmail.com>

--- test/Microsoft.TestPlatform.TestHostProvider.UnitTests/Hosting/TestHostManagerCallbacksTests.cs
+++ test/Microsoft.TestPlatform.TestHostProvider.UnitTests/Hosting/TestHostManagerCallbacksTests.cs
@@ -2,9 +2,13 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 
 using Microsoft.TestPlatform.TestHostProvider.Hosting;
+using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions;
+using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions.Interfaces;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestPlatform.TestHostProvider.Hosting.UnitTests;
@@ -93,5 +97,104 @@ public class TestHostManagerCallbacksTests
         TestHostManagerCallbacks.ErrorReceivedCallback(_testHostProcessStdError, "4");
 
         Assert.AreEqual("1234" + Environment.NewLine.Substring(0, 1), _testHostProcessStdError.ToString());
+    }
+
+    [TestMethod]
+    public void ErrorReceivedCallbackShouldNotCrashIfInvalidProcessHandle()
+    {
+        var handleProcessHelper = new InvalidHandleProcessHelper();
+        bool onHostExitedCalled = false;
+        TestHostManagerCallbacks.ExitCallBack(handleProcessHelper, null, new StringBuilder(),
+            hostProviderEventArgs =>
+            {
+                onHostExitedCalled = true;
+                Assert.AreEqual(-1, hostProviderEventArgs.ErrroCode);
+            });
+
+        Assert.IsTrue(handleProcessHelper.TryGetExitCodeCalled, "TryGetExitCodeCalled was not called");
+        Assert.IsTrue(onHostExitedCalled, "onHostExited was not called");
+    }
+
+    private class InvalidHandleProcessHelper : IProcessHelper
+    {
+        public bool TryGetExitCodeCalled { get; private set; }
+
+        public PlatformArchitecture GetCurrentProcessArchitecture()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? GetCurrentProcessFileName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCurrentProcessId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCurrentProcessLocation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetNativeDllDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public PlatformArchitecture GetProcessArchitecture(int processId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public nint GetProcessHandle(int processId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetProcessId(object? process)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProcessName(int processId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? GetTestEngineDirectory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object LaunchProcess(string processPath, string? arguments, string? workingDirectory, IDictionary<string, string?>? envVariables, Action<object?, string?>? errorCallback, Action<object?>? exitCallBack, Action<object?, string?>? outputCallBack)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetExitCallback(int processId, Action<object?>? callbackAction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TerminateProcess(object? process)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetExitCode(object? process, out int exitCode)
+        {
+            var err = new COMException("Invalid handle", unchecked((int)0x80070006));
+            typeof(COMException).GetProperty("HResult")!.SetValue(err, unchecked((int)0x80070006));
+            TryGetExitCodeCalled = true;
+            throw err;
+        }
+
+        public void WaitForProcessExit(object? process)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
