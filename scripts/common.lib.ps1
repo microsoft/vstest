839f0c349d616f5bf2f976cab6e4492610d1a537
Jakub Jareš <me@jakubjares.com>
2023-02-22 08:54:41 +0100

    [rel/17.0] Update Newtonsoft.Json to 13.0.1 (#4309
    * Fix build
    
    * Upgrade to Newtonsoft.Json 13.0.1 (#3815)
    
    * Upgrade arcade
    
    * update vs image used to build
    
    * Add 3.1.32 dotnet runtime
    
    * Ignore acceptance test for multi targetting, we did not touch that, it relies on an edge case, and it does not depend on newtonsoft
    
    * Bump version
    
    * Fix version

--- scripts/common.lib.ps1
+++ scripts/common.lib.ps1
@@ -98,7 +98,7 @@ function Install-DotNetCli
     Write-Log "Install-DotNetCli: Get the latest dotnet cli toolset..."
     $dotnetInstallPath = Join-Path $env:TP_TOOLS_DIR "dotnet"
     New-Item -ItemType directory -Path $dotnetInstallPath -Force | Out-Null
-    & $dotnetInstallScript -Channel 6.0 -Quality Preview -InstallDir $dotnetInstallPath -Version $env:DOTNET_CLI_VERSION
+    & $dotnetInstallScript -Channel 6.0.100 -InstallDir $dotnetInstallPath -Version $env:DOTNET_CLI_VERSION
     
     & $dotnetInstallScript -InstallDir "$dotnetInstallPath" -Runtime 'dotnet' -Version '2.1.0' -Channel '2.1.0' -Architecture x64 -NoPath
     $env:DOTNET_ROOT= $dotnetInstallPath
@@ -107,9 +107,11 @@ function Install-DotNetCli
     ${env:DOTNET_ROOT(x86)} = "${dotnetInstallPath}_x86"
     
     & $dotnetInstallScript -InstallDir "$dotnetInstallPath" -Runtime 'dotnet' -Version '3.1.0' -Channel '3.1.0' -Architecture x64 -NoPath
+    & $dotnetInstallScript -InstallDir "$dotnetInstallPath" -Runtime 'dotnet' -Version '3.1.32' -Channel '3.1.0' -Architecture x64 -NoPath
     $env:DOTNET_ROOT= $dotnetInstallPath
 
     & $dotnetInstallScript -InstallDir "${dotnetInstallPath}_x86" -Runtime 'dotnet' -Version '3.1.0' -Channel '3.1.0' -Architecture x86 -NoPath
+    & $dotnetInstallScript -InstallDir "${dotnetInstallPath}_x86" -Runtime 'dotnet' -Version '3.1.32' -Channel '3.1.0' -Architecture x86 -NoPath
     ${env:DOTNET_ROOT(x86)} = "${dotnetInstallPath}_x86"
 
     & $dotnetInstallScript -InstallDir "$dotnetInstallPath" -Runtime 'dotnet' -Version '5.0.1' -Channel '5.0.1' -Architecture x64 -NoPath
