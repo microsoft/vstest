6a5cfb5f448e7e052b76b24385f80c129b92277e
Simon Cropp <simon.cropp@gmail.com>
2023-06-06 21:07:04 +1000

    Update TestPluginCache.cs

--- src/Microsoft.TestPlatform.Common/ExtensionFramework/TestPluginCache.cs
+++ src/Microsoft.TestPlatform.Common/ExtensionFramework/TestPluginCache.cs
@@ -128,7 +128,7 @@ public class TestPluginCache
             return TestExtensions.GetTestExtensionCache<TPluginInfo>();
         }
 
-        Dictionary<string, TPluginInfo>? pluginInfos;
+        Dictionary<string, TPluginInfo>? pluginInfos = null;
         SetupAssemblyResolver(null);
 
         // Some times TestPlatform.core.dll assembly fails to load in the current appdomain (from devenv.exe).
