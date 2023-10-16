70b72d44b95e3a6b6901ad5e2a4c987728096911
Codrin Poienaru <cvpoienaru@gmail.com>
2023-10-16 17:29:41 +0200

    Fixes

--- eng/verify-version.ps1
+++ eng/verify-version.ps1
@@ -4,72 +4,46 @@ Param(
     [string] $VSTestExe
 )
 
-$Script:RootDir = (Get-Item (Split-Path $MyInvocation.MyCommand.Path)).Parent.FullName
-
-function Extract-VSTestVersion {
-    param (
-        [string]$VSTestExe
-    )
-
-    $outputFile = Join-Path $RootDir "output.txt"
-
-    Invoke-Expression $VSTestExe > $outputFile 2>&1
-    $firstLine = Get-Content -Path $outputFile -TotalCount 1
-    Remove-Item $outputFile
-
-    $version = ""
-    $matchFound = $firstLine -match "(?<=Version\s+).*"
-    if ($matchFound) {
-        $version = $matches[0]
-    }
-
-    return $version
-}
-
 function Match-VersionAgainstBranch {
     param (
         [string]$VSTestVersion,
         [string]$BranchName
     )
 
-    $versionIsRTM = $VSTestVersion -match "^\d+\.\d+\.\d+( \(x\d+\))?$"
-    $versionIsRelease = $VSTestVersion -match "^\d+\.\d+\.\d+\-release\-\d{8}\-\d{2}( \(x\d+\))?$"
-    $versionIsPreview = $VSTestVersion -match "^\d+\.\d+\.\d+\-preview\-\d{8}\-\d{2}( \(x\d+\))?$"
+    # Output useful info.
+    Write-Host "VSTest Product Version: `"$VSTestVersion`""
+    Write-Host "Current Branch: `"$BranchName`""
+
+    $versionIsRTM = $VSTestVersion -match "^\d+\.\d+\.\d+$"
+    $versionIsRelease = $VSTestVersion -match "^\d+\.\d+\.\d+\-release\-\d{8}\-\d{2}$"
+    $versionIsPreview = $VSTestVersion -match "^\d+\.\d+\.\d+\-preview\-\d{8}\-\d{2}$"
 
     $isReleaseBranch = $BranchName -like "rel/*"
     $isPreviewBranch = $BranchName -like "main"
 
     if (!$isReleaseBranch -and !$isPreviewBranch)
     {
-        Write-Error "Branch `"$CurrentBranch`" is neither a `"release`" branch, nor a `"preview`" branch (main)."
-        Exit 1
+        Write-Host "Skipping check since branch is neither `"release`" nor `"preview`""
+        return
     }
+
+    Write-Host "Matching branch against product version ... "
     if ($isReleaseBranch -and !$versionIsRTM -and !$versionIsRelease)
     {
         Write-Error "Release version `"$VSTestVersion`" should either be RTM, or contain a `"release`" suffix."
-        Exit 2
+        Exit 1
     }
     if ($isPreviewBranch -and !$versionIsPreview)
     {
         Write-Error "Preview version `"$VSTestVersion`" should contain a `"preview`" suffix."
-        Exit 3
+        Exit 2
     }
+
+    Write-Host "Branch and version check was successful."
+    Exit 0
 }
 
-$Script:VSTestOutputVersion = Extract-VSTestVersion -VSTestExe $VSTestExe
 $Script:VSTestProductVersion = (Get-Item $VSTestExe).VersionInfo.ProductVersion
 $Script:CurrentBranch = git branch --show-current
 
-# Output useful info.
-Write-Host "VSTest Output Version: `"$VSTestOutputVersion`""
-Write-Host "VSTest Product Version: `"$VSTestProductVersion`""
-Write-Host "Current Branch: `"$CurrentBranch`""
-
-Write-Host "Matching branch against output version ... "
-Match-VersionAgainstBranch -VSTestVersion $VSTestOutputVersion -BranchName $CurrentBranch
-
-Write-Host "Matching branch against product version ... "
 Match-VersionAgainstBranch -VSTestVersion $VSTestProductVersion -BranchName $CurrentBranch
-
-Write-Host "Branch and version check was successful."
-Exit 0
