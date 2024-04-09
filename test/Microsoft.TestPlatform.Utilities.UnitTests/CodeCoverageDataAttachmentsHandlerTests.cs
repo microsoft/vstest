443a9f6b007de77b4bfd660c66dc130a2f0426fa
Jakub Jareš <me@jakubjares.com>
2024-04-09 17:31:01 +0200

    Which one copies them first?

--- test/Microsoft.TestPlatform.Utilities.UnitTests/CodeCoverageDataAttachmentsHandlerTests.cs
+++ test/Microsoft.TestPlatform.Utilities.UnitTests/CodeCoverageDataAttachmentsHandlerTests.cs
@@ -50,7 +50,7 @@ public class CodeCoverageDataAttachmentsHandlerTests
 #endif
     }
 
-#if NETFRAMEWORK
+#if !NETFRAMEWORK
     [ClassInitialize]
     public static void ClassInitialize(TestContext context)
     {
