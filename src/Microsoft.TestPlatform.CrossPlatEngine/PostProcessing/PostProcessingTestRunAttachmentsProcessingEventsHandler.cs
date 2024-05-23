e81b6a8bc7e0dcd0571ae64172099830ab3f5f5b
Simon Cropp <simon.cropp@gmail.com>
2024-05-23 19:22:12 +1000

    Update PostProcessingTestRunAttachmentsProcessingEventsHandler.cs

--- src/Microsoft.TestPlatform.CrossPlatEngine/PostProcessing/PostProcessingTestRunAttachmentsProcessingEventsHandler.cs
+++ src/Microsoft.TestPlatform.CrossPlatEngine/PostProcessing/PostProcessingTestRunAttachmentsProcessingEventsHandler.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
