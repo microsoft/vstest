08d5518d70de7dc642c81de5a4f566e859bbc3ea
Jakub Jareš <me@jakubjares.com>
2024-07-08 11:58:51 +0200

    Verify architecture and version of produced exes

--- eng/verify-nupkgs-exe-version.ps1
+++
@@ -1,8 +0,0 @@
-Get-ChildItem  S:\p\vstest3\artifacts\packages\Debug\Shipping -Filter vstest.console.exe -Recurse -Force | ForEach-Object {
-    if ($_.VersionInfo.ProductVersion.Contains("+")) {
-        throw "Some files contain '+' in the ProductVersion, this breaks DTAAgent in AzDO."
-    }
-    else {
-        "$_ version $($_.VersionInfo.ProductVersion) is ok."
-    }
-} 
