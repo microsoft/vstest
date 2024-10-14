7875cb7e461578576150a3683bb3479cf9b203e9
Jakub Jareš <me@jakubjares.com>
2024-10-14 17:51:03 +0200

    Revert "Add msdia for arm64 into nuget (#10382)"
    This reverts commit ba273e1848c009dd0cd5517e483505830b615712.

--- eng/verify-nupkgs.ps1
+++ eng/verify-nupkgs.ps1
@@ -19,7 +19,7 @@ function Verify-Nuget-Packages {
     $expectedNumOfFiles = @{
         "Microsoft.CodeCoverage"                      = 59;
         "Microsoft.NET.Test.Sdk"                      = 15;
-        "Microsoft.TestPlatform"                      = 609;
+        "Microsoft.TestPlatform"                      = 608;
         "Microsoft.TestPlatform.Build"                = 20;
         "Microsoft.TestPlatform.CLI"                  = 471;
         "Microsoft.TestPlatform.Extensions.TrxLogger" = 34;
