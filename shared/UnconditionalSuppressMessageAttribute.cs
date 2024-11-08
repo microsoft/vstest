05d5146c5fbff75d814122f325045aadb207958b
Youssef1313 <youssefvictor00@gmail.com>
2024-11-08 06:16:39 +0100

    File header comment

--- shared/UnconditionalSuppressMessageAttribute.cs
+++ shared/UnconditionalSuppressMessageAttribute.cs
@@ -1,4 +1,7 @@
-﻿#if !NET5_0_OR_GREATER
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#if !NET5_0_OR_GREATER
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
