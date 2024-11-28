172837b42f3b2bdfaee5ebff06cfd18e6947aa23
Jakub Jareš <me@jakubjares.com>
2024-11-28 17:15:54 +0100

    solve conflicts

--- eng/verify-nupkgs.ps1
+++ eng/verify-nupkgs.ps1
@@ -19,7 +19,7 @@ function Verify-Nuget-Packages {
     $expectedNumOfFiles = @{
         "Microsoft.CodeCoverage"                      = 59;
         "Microsoft.NET.Test.Sdk"                      = 15;
-        "Microsoft.TestPlatform"                      = 614;
+        "Microsoft.TestPlatform"                      = 618;
         "Microsoft.TestPlatform.Build"                = 20;
         "Microsoft.TestPlatform.CLI"                  = 477;
         "Microsoft.TestPlatform.Extensions.TrxLogger" = 34;
