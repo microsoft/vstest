2e8900a919b068300eab7348daed994e0e82dd33
Jakub Jareš <me@jakubjares.com>
2025-01-17 15:38:52 +0100

    fixes

--- src/Microsoft.TestPlatform.Extensions.TrxLogger/ObjectModel/PropertyCollection.cs
+++ src/Microsoft.TestPlatform.Extensions.TrxLogger/ObjectModel/PropertyCollection.cs
@@ -1,125 +1,27 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using System.Text;
 
 using Microsoft.TestPlatform.Extensions.TrxLogger.Utility;
-using Microsoft.TestPlatform.Extensions.TrxLogger.XML;
 using Microsoft.VisualStudio.TestPlatform.Extensions.TrxLogger;
 
 namespace Microsoft.TestPlatform.Extensions.TrxLogger.ObjectModel;
 
-#region TraitItem
-/// <summary>
-/// Stores a string which categorizes the Test
-/// </summary>
-internal sealed class Property : IXmlTestStore
-{
-    [StoreXmlSimpleField(Location = "@Key", DefaultValue = "")]
-    private readonly string _key = string.Empty;
-    [StoreXmlSimpleField(Location = "@Value", DefaultValue = "")]
-    private readonly string _value = string.Empty;
-
-    /// <summary>
-    /// Create a new item with the category set
-    /// </summary>
-    public Property(string key, string value)
-    {
-        // Treat null as empty.
-
-        _key = StripIllegalChars(key);
-        _value = StripIllegalChars(value);
-    }
-
-    /// <summary>
-    /// Gets the category for this Trait
-    /// </summary>
-    public string Trait
-    {
-        get
-        {
-            return _key;
-        }
-    }
-
-    private static string StripIllegalChars(string category)
-    {
-        string ret = category.Trim();
-        ret = ret.Replace("&", string.Empty);
-        ret = ret.Replace("|", string.Empty);
-        ret = ret.Replace("!", string.Empty);
-        ret = ret.Replace(",", string.Empty);
-        return ret;
-    }
-
-    /// <summary>
-    /// Compare the values of the items
-    /// </summary>
-    /// <param name="other">Value being compared to.</param>
-    /// <returns>True if the values are the same and false otherwise.</returns>
-    public override bool Equals(object? other)
-    {
-        if (other is not Property otherItem)
-        {
-            return false;
-        }
-
-        TPDebug.Assert(_key != null, "category is null");
-        return string.Equals(_key, otherItem._key, StringComparison.OrdinalIgnoreCase);
-    }
-
-    /// <summary>
-    /// Convert the category name to a hashcode
-    /// </summary>
-    /// <returns>Hashcode of the category.</returns>
-    public override int GetHashCode()
-    {
-        TPDebug.Assert(_key != null, "category is null");
-        return _key.ToUpperInvariant().GetHashCode();
-    }
-
-    /// <summary>
-    /// Convert the category name to a string
-    /// </summary>
-    /// <returns>The category.</returns>
-    public override string ToString()
-    {
-        TPDebug.Assert(_key != null, "category is null");
-        return _key;
-    }
-
-    #region IXmlTestStore Members
-
-    /// <summary>
-    /// Saves the class under the XmlElement.
-    /// </summary>
-    /// <param name="element"> XmlElement element </param>
-    /// <param name="parameters"> XmlTestStoreParameters parameters</param>
-    public void Save(System.Xml.XmlElement element, XmlTestStoreParameters? parameters)
-    {
-        new XmlPersistence().SaveSingleFields(element, this, parameters);
-    }
-
-    #endregion
-}
-#endregion
-
-#region TraitItemCollection
 /// <summary>
 /// A collection of strings which categorize the test.
 /// </summary>
 internal sealed class PropertyCollection : EqtBaseCollection<Property>
 {
     /// <summary>
-    /// Creates an empty TraitItemCollection.
+    /// Creates an empty PropertyCollection.
     /// </summary>
     public PropertyCollection()
     {
     }
 
     /// <summary>
-    /// Adds the category.
+    /// Adds the property.
     /// </summary>
     /// <param name="key">Key to be added.</param>
     /// <param name="value">Value to be added.</param>
@@ -129,9 +31,9 @@ internal sealed class PropertyCollection : EqtBaseCollection<Property>
     }
 
     /// <summary>
-    /// Adds the category.
+    /// Adds the property.
     /// </summary>
-    /// <param name="item">Category to be added.</param>
+    /// <param name="item">Property to be added.</param>
     public override void Add(Property item)
     {
         EqtAssert.ParameterNotNull(item, nameof(item));
@@ -144,7 +46,7 @@ internal sealed class PropertyCollection : EqtBaseCollection<Property>
     }
 
     /// <summary>
-    /// Convert the TraitItemCollection to a string.
+    /// Convert the PropertyCollection to a string.
     /// each item is surrounded by a comma (,)
     /// </summary>
     /// <returns></returns>
@@ -165,7 +67,7 @@ internal sealed class PropertyCollection : EqtBaseCollection<Property>
     }
 
     /// <summary>
-    /// Convert the TraitItemCollection to an array of strings.
+    /// Convert the PropertyCollection to an array of strings.
     /// </summary>
     /// <returns>Array of strings containing the test categories.</returns>
     public string[] ToArray()
@@ -231,4 +133,3 @@ internal sealed class PropertyCollection : EqtBaseCollection<Property>
         return base.GetHashCode();
     }
 }
-#endregion
