77244c9c584e3964a423580ce747a0eb2bf67f97
Youssef1313 <youssefvictor00@gmail.com>
2025-06-03 17:23:13 +0200

    Fix

--- test/Microsoft.TestPlatform.CrossPlatEngine.UnitTests/Discovery/DiscovererEnumeratorTests.cs
+++ test/Microsoft.TestPlatform.CrossPlatEngine.UnitTests/Discovery/DiscovererEnumeratorTests.cs
@@ -758,6 +758,7 @@ public class DiscovererEnumeratorTests
     }
 
     [FileExtension(".dll")]
+    [FileExtension(".exe")]
     [DefaultExecutorUri("discoverer://manageddlldiscoverer")]
     [Category("managed")]
     private class ManagedDllTestDiscoverer : DllTestDiscoverer
@@ -833,7 +834,7 @@ public class DiscovererEnumeratorTests
             var shouldTestDiscovered = false;
             foreach (var source in sources)
             {
-                if (source.Equals("native.dll") || source.Equals("managed.dll") || source.EndsWith("CrossPlatEngine.UnitTests.dll"))
+                if (source.Equals("native.dll") || source.Equals("managed.dll") || source.EndsWith("CrossPlatEngine.UnitTests.dll") || source.EndsWith("CrossPlatEngine.UnitTests.exe"))
                 {
                     shouldTestDiscovered = true;
                     break;
