34eda29b73427ad8f7eb75f57049dba4aa8a17b3
Youssef Victor <youssefvictor00@gmail.com>
2025-07-01 10:46:25 +0200

    Fix test

--- test/Microsoft.TestPlatform.Utilities.UnitTests/InferRunSettingsHelperTests.cs
+++ test/Microsoft.TestPlatform.Utilities.UnitTests/InferRunSettingsHelperTests.cs
@@ -53,7 +53,7 @@ public class InferRunSettingsHelperTests
         Action action = () => InferRunSettingsHelper.UpdateRunSettingsWithUserProvidedSwitches(xmlDocument, Architecture.X86, Framework.DefaultFramework, "temp");
 
         var exception = Assert.ThrowsExactly<XmlException>(action);
-        Assert.AreEqual("An error occurred while loading the settings.  Error: Invalid setting 'RunConfiguration'. Invalid value 'foo' specified for 'TargetPlatform'.", exception.Message);
+        Assert.AreEqual("An error occurred while loading the settings.  Error: Invalid setting 'RunConfiguration'. Invalid value 'foo' specified for 'TargetPlatform'..", exception.Message);
     }
 
     [TestMethod]
