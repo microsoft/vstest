3c27ccf68b0a16443dff6b83bbb4d6a3d2ecc0e8
Jakub Jareš <me@jakubjares.com>
2025-08-13 10:04:46 +0200

    remove collectors

--- eng/verify-nupkgs.ps1
+++ eng/verify-nupkgs.ps1
@@ -20,7 +20,7 @@ function Verify-Nuget-Packages {
         "Microsoft.CodeCoverage"                      = 75
         "Microsoft.NET.Test.Sdk"                      = 25
         "Microsoft.TestPlatform"                      = 538
-        "Microsoft.VisualStudio.TestTools.TestPlatform.V2.CLI" = 382
+        "Microsoft.VisualStudio.TestTools.TestPlatform.V2.CLI" = 380
         "Microsoft.TestPlatform.Build"                = 20
         "Microsoft.TestPlatform.CLI"                  = 481
         "Microsoft.TestPlatform.Extensions.TrxLogger" = 34
