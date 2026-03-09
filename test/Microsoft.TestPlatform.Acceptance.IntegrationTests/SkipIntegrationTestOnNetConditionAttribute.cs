79f1475e197fdd192a3eb3599b9ca99de44987a2
nohwnd <me@jakubjares.com>
2026-03-09 10:00:09 +0100

    Add conditional attribute to skip tests

---
+++ test/Microsoft.TestPlatform.Acceptance.IntegrationTests/SkipIntegrationTestOnNetConditionAttribute.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Runtime.InteropServices;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.TestPlatform.AcceptanceTests;
+
+public class SkipIntegrationTestOnNetConditionAttribute : ConditionBaseAttribute
+{
+    private readonly bool _include;
+
+    public SkipIntegrationTestOnNetConditionAttribute() : base(ConditionMode.Include)
+    {
+        _include = RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework");
+        IgnoreMessage = "Test was skipped to avoid duplication of the same out-of-process tests between .NET and .NET Framework.";
+    }
+
+    public override string GroupName => "tfm";
+
+    public override bool IsConditionMet => _include;
+}
