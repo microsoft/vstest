microsoft/vstest

commit cd3cc9e3e2cbe9c6ec9d0e78b06f1c553ee93dec

sync: pick up upstream improvements from main
- Task.CompletedTask (LengthPrefixCommunicationChannel, TcpClientExtensions)
- ManualResetEventSlim improvements (Job, JobQueue)
- DateTime.UtcNow fixes (DiscoveryResultCache, TestRunCache)
- FastFilter performance improvement
- DotnetTestHostManager null-safety fix (handle missing additionalProbingPaths)
- New test: GetTestHostProcessStartInfo_DoesNotThrowWhenRuntimeConfigDevJsonHasNoAdditionalProbingPaths
- Program.cs comment, Resources.resx update, XLF translations

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

src/Microsoft.TestPlatform.CrossPlatEngine/Discovery/DiscoveryResultCache.cs
@@ -57,7 +57,7 @@ internal class DiscoveryResultCache
 
         _cacheSize = cacheSize;
         _onReportTestCases = onReportTestCases;
-        _lastUpdate = DateTime.Now;
+        _lastUpdate = DateTime.UtcNow;
         _cacheTimeout = discoveredTestEventTimeout;
 
         _tests = new List<TestCase>();
@@ -107,13 +107,13 @@ internal class DiscoveryResultCache
 
             // Send test cases when the specified cache size has been reached or
             // after the specified cache timeout has been hit.
-            var timeDelta = DateTime.Now - _lastUpdate;
+            var timeDelta = DateTime.UtcNow - _lastUpdate;
             if (_tests.Count >= _cacheSize || (timeDelta > _cacheTimeout && _tests.Count > 0))
             {
                 // Pass on the buffer to the listener and clear the old one
                 _onReportTestCases(_tests);
                 _tests = new List<TestCase>();
-                _lastUpdate = DateTime.Now;
+                _lastUpdate = DateTime.UtcNow;
 
                 EqtTrace.Verbose("DiscoveryResultCache.AddTest: Notified the onReportTestCases callback.");
             }

src/Microsoft.TestPlatform.CrossPlatEngine/Execution/TestRunCache.cs
@@ -91,7 +91,7 @@ internal class TestRunCache : ITestRunCache
 
         _cacheSize = cacheSize;
         _onCacheHit = onCacheHit;
-        _lastUpdate = DateTime.Now;
+        _lastUpdate = DateTime.UtcNow;
         _cacheTimeout = cacheTimeout;
         _inProgressTests = new Collection<TestCase>();
         _testResults = new Collection<TestResult>();
@@ -311,7 +311,7 @@ internal class TestRunCache : ITestRunCache
         {
             // Send results when the specified cache size has been reached or
             // after the specified cache timeout has been hit.
-            var timeDelta = DateTime.Now - _lastUpdate;
+            var timeDelta = DateTime.UtcNow - _lastUpdate;
 
             var inProgressTestsCount = _inProgressTests.Count;
 
@@ -339,7 +339,7 @@ internal class TestRunCache : ITestRunCache
         _onCacheHit(TestRunStatistics, _testResults, _inProgressTests);
         _testResults = new Collection<TestResult>();
         _inProgressTests = new Collection<TestCase>();
-        _lastUpdate = DateTime.Now;
+        _lastUpdate = DateTime.UtcNow;
 
         // Reset the timer
         _timer.Change(_cacheTimeout, _cacheTimeout);

src/Microsoft.TestPlatform.CommunicationUtilities/LengthPrefixCommunicationChannel.cs
@@ -67,7 +67,7 @@ public class LengthPrefixCommunicationChannel : ICommunicationChannel
             throw new CommunicationException("Unable to send data over channel.", ex);
         }
 
-        return Task.FromResult(0);
+        return Task.CompletedTask;
     }
 
     /// <inheritdoc />
@@ -101,7 +101,7 @@ public class LengthPrefixCommunicationChannel : ICommunicationChannel
             EqtTrace.Verbose("LengthPrefixCommunicationChannel.Send: BaseStream was disposed. {0}", ex);
         }
 
-        return Task.FromResult(0);
+        return Task.CompletedTask;
     }
 
     /// <inheritdoc />

src/Microsoft.TestPlatform.CommunicationUtilities/TcpClientExtensions.cs
@@ -110,7 +110,7 @@ internal static class TcpClientExtensions
 
         EqtTrace.Verbose("TcpClientExtensions.MessageLoopAsync: exiting MessageLoopAsync remoteEndPoint: {0} localEndPoint: {1}", remoteEndPoint, localEndPoint);
 
-        return Task.FromResult(0);
+        return Task.CompletedTask;
     }
 
     /// <summary>

src/Microsoft.TestPlatform.CoreUtilities/Utilities/Job.cs
@@ -39,16 +39,7 @@ internal class Job<TPayload>
     /// <summary>
     /// Gets a special job that indicates the queue should shutdown.
     /// </summary>
-    public static Job<TPayload> ShutdownJob
-    {
-        get
-        {
-            var shutdownJob = new Job<TPayload>();
-            shutdownJob.Shutdown = true;
-
-            return shutdownJob;
-        }
-    }
+    public static Job<TPayload> ShutdownJob => new() { Shutdown = true };
 
     /// <summary>
     /// Gets the job to be processed.
@@ -63,7 +54,7 @@ internal class Job<TPayload>
     /// <summary>
     /// Gets the signal that this job is being processed.
     /// </summary>
-    public ManualResetEvent? WaitManualResetEvent { get; private set; }
+    public ManualResetEventSlim? WaitManualResetEvent { get; private set; }
 
     /// <summary>
     /// Gets the size of this job instance. This is used to manage the total size of Job Queue.
@@ -75,14 +66,11 @@ internal class Job<TPayload>
     /// </summary>
     /// <param name="waitEvent"> The wait Event. </param>
     /// <returns> The wait job. </returns>
-    public static Job<TPayload> CreateWaitJob(ManualResetEvent waitEvent)
+    public static Job<TPayload> CreateWaitJob(ManualResetEventSlim waitEvent)
     {
         ValidateArg.NotNull(waitEvent, nameof(waitEvent));
 
-        var waitJob = new Job<TPayload>();
-        waitJob.WaitManualResetEvent = waitEvent;
-
-        return waitJob;
+        return new Job<TPayload> { WaitManualResetEvent = waitEvent };
     }
 
 }

src/Microsoft.TestPlatform.CoreUtilities/Utilities/JobQueue.cs
@@ -38,7 +38,7 @@ public class JobQueue<T> : IDisposable
     /// <summary>
     /// Signaled when a job is added to the queue.  Used to wakeup the background thread.
     /// </summary>
-    private readonly ManualResetEvent _jobAdded;
+    private readonly ManualResetEventSlim _jobAdded;
 
     /// <summary>
     /// The maximum number of jobs the job queue may hold.
@@ -64,7 +64,7 @@ public class JobQueue<T> : IDisposable
     /// Used to pause and resume processing of the queue.  By default the manual reset event is
     /// set so the queue can continue processing.
     /// </summary>
-    private readonly ManualResetEvent _queueProcessing;
+    private readonly ManualResetEventSlim _queueProcessing;
 
     /// <summary>
     /// The background thread which is processing the jobs.  Used when disposing to wait
@@ -122,10 +122,8 @@ public class JobQueue<T> : IDisposable
 
         // Initialize defaults.
         _jobsQueue = new Queue<Job<T>>();
-        _jobAdded = new ManualResetEvent(false);
-        _queueProcessing = new ManualResetEvent(true);
-        _currentNumberOfBytesQueueIsHolding = 0;
-        _isDisposed = false;
+        _jobAdded = new ManualResetEventSlim(false);
+        _queueProcessing = new ManualResetEventSlim(true);
 
         // Save off the arguments.
         _displayName = displayName;
@@ -183,13 +181,13 @@ public class JobQueue<T> : IDisposable
         CheckDisposed();
 
         // Create the wait job.
-        using var waitEvent = new ManualResetEvent(false);
+        using var waitEvent = new ManualResetEventSlim(false);
         var waitJob = Job<T>.CreateWaitJob(waitEvent);
 
         // Queue the wait job and wait for it to be processed.
         InternalQueueJob(waitJob);
 
-        waitEvent.WaitOne();
+        waitEvent.Wait();
     }
 
     /// <summary>
@@ -210,7 +208,7 @@ public class JobQueue<T> : IDisposable
         }
 
         // If the queue is paused, then throw.
-        if (!_queueProcessing.WaitOne(0))
+        if (!_queueProcessing.IsSet)
         {
             throw new InvalidOperationException(
                 string.Format(CultureInfo.CurrentCulture, Resources.QueuePausedDisposeError, _displayName));
@@ -297,7 +295,7 @@ public class JobQueue<T> : IDisposable
 
         do
         {
-            _jobAdded.WaitOne();
+            _jobAdded.Wait();
 
             // Pull all of the current jobs out of the queue.
             List<Job<T>> jobs = new();
@@ -332,7 +330,7 @@ public class JobQueue<T> : IDisposable
             foreach (var job in jobs)
             {
                 // Wait for the queue to be open (not paused) and process the job.
-                _queueProcessing.WaitOne();
+                _queueProcessing.Wait();
 
                 // If this is a wait job, signal the manual reset event and continue.
                 if (job.WaitManualResetEvent != null)

src/Microsoft.TestPlatform.Filter.Source/FastFilter.cs
@@ -80,13 +80,15 @@ internal sealed class FastFilter
 #endif
 
         bool matched = false;
-        foreach (var name in FilterProperties.Keys)
+        foreach (var kvp in FilterProperties)
         {
+            var filterValues = kvp.Value;
+
             // Reserved keyword: "None" matches tests with no value for this property (uncategorized).
-            bool hasNoneFilter = FilterProperties[name].Contains(Condition.NoneFilterValue);
+            bool hasNoneFilter = filterValues.Contains(Condition.NoneFilterValue);
 
             // If there is no value corresponding to given name, treat it as unmatched unless filtering for "None".
-            if (!TryGetPropertyValue(name, propertyValueProvider, out var singleValue, out var multiValues))
+            if (!TryGetPropertyValue(kvp.Key, propertyValueProvider, out var singleValue, out var multiValues))
             {
                 if (hasNoneFilter)
                 {
@@ -100,12 +102,33 @@ internal sealed class FastFilter
             if (singleValue != null)
             {
                 var value = PropertyValueRegex == null ? singleValue : ApplyRegex(singleValue);
-                matched = value != null && FilterProperties[name].Contains(value);
+                matched = value != null && filterValues.Contains(value);
             }
             else if (multiValues is { Length: > 0 })
             {
-                var values = PropertyValueRegex == null ? multiValues : multiValues?.Select(value => ApplyRegex(value));
-                matched = values?.Any(result => result != null && FilterProperties[name].Contains(result)) == true;
+                if (PropertyValueRegex == null)
+                {
+                    foreach (var result in multiValues)
+                    {
+                        if (result != null && filterValues.Contains(result))
+                        {
+                            matched = true;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (var result in multiValues)
+                    {
+                        var transformed = ApplyRegex(result);
+                        if (transformed != null && filterValues.Contains(transformed))
+                        {
+                            matched = true;
+                            break;
+                        }
+                    }
+                }
             }
             else if (hasNoneFilter)
             {

src/Microsoft.TestPlatform.TestHostProvider/Hosting/DotnetTestHostManager.cs
@@ -955,34 +955,43 @@ public class DotnetTestHostManager : ITestRuntimeProvider2
                 {
 #if NETCOREAPP
                     using var doc = JsonDocument.Parse(stream);
-                    var runtimeOptions = doc.RootElement.GetProperty("runtimeOptions");
-                    var additionalProbingPaths = runtimeOptions.GetProperty("additionalProbingPaths");
-                    foreach (var x in additionalProbingPaths.EnumerateArray())
-                    {
-                        EqtTrace.Verbose("DotnetTestHostmanager: Looking for path {0} in folder {1}", testHostPath, x.GetString());
-                        string testHostFullPath;
-                        try
-                        {
-                            testHostFullPath = Path.Combine(x.GetString()!, testHostPath);
-                        }
-                        catch (ArgumentException)
-                        {
-                            // https://github.com/Microsoft/vstest/issues/847
-                            // skip any invalid paths and continue checking the others
-                            continue;
-                        }
 
-                        if (_fileHelper.Exists(testHostFullPath))
+                    if (doc.RootElement.TryGetProperty("runtimeOptions", out var runtimeOptions) &&
+                        runtimeOptions.TryGetProperty("additionalProbingPaths", out var additionalProbingPaths))
+                    {
+                        foreach (var x in additionalProbingPaths.EnumerateArray())
                         {
-                            EqtTrace.Verbose("DotnetTestHostmanager: Found testhost.dll in {0}", testHostFullPath);
-                            return testHostFullPath;
+                            EqtTrace.Verbose("DotnetTestHostmanager: Looking for path {0} in folder {1}", testHostPath, x.GetString());
+                            string testHostFullPath;
+                            try
+                            {
+                                testHostFullPath = Path.Combine(x.GetString()!, testHostPath);
+                            }
+                            catch (ArgumentException)
+                            {
+                                // https://github.com/Microsoft/vstest/issues/847
+                                // skip any invalid paths and continue checking the others
+                                continue;
+                            }
+
+                            if (_fileHelper.Exists(testHostFullPath))
+                            {
+                                EqtTrace.Verbose("DotnetTestHostmanager: Found testhost.dll in {0}", testHostFullPath);
+                                return testHostFullPath;
+                            }
                         }
                     }
 #else
                     using var reader = new StreamReader(stream);
                     var parsed = Json.Deserialize(reader) as IDictionary<string, object>;
-                    var runtimeOpts = parsed?["runtimeOptions"] as IDictionary<string, object>;
-                    var probingPaths = runtimeOpts?["additionalProbingPaths"] as IList<object>;
+                    var runtimeOpts = parsed is not null && parsed.TryGetValue("runtimeOptions", out var runtimeOptsObj)
+                        ? runtimeOptsObj as IDictionary<string, object>
+                        : null;
+
+                    var probingPaths = runtimeOpts is not null && runtimeOpts.TryGetValue("additionalProbingPaths", out var probingPathsObj)
+                        ? probingPathsObj as IList<object>
+                        : null;
+
                     if (probingPaths is not null)
                     {
                         foreach (var x in probingPaths)

commit 7439b6958d1239feff92e6c0438a4e11bee561fd

fix: align DiscoveryResultCache and TestRunCache with main to resolve merge conflict
Pick up InitialCapacity pre-allocation from main (#16165) to resolve the
3-way merge conflict caused by the cd3cc9e sync commit applying the
DateTime.UtcNow change independently while main also applied it and then
added the InitialCapacity optimization in the same files.

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

src/Microsoft.TestPlatform.CrossPlatEngine/Discovery/DiscoveryResultCache.cs
@@ -60,7 +60,7 @@ internal class DiscoveryResultCache
         _lastUpdate = DateTime.UtcNow;
         _cacheTimeout = discoveredTestEventTimeout;
 
-        _tests = new List<TestCase>();
+        _tests = new List<TestCase>(InitialCapacity(cacheSize));
         TotalDiscoveredTests = 0;
     }
 
@@ -112,11 +112,13 @@ internal class DiscoveryResultCache
             {
                 // Pass on the buffer to the listener and clear the old one
                 _onReportTestCases(_tests);
-                _tests = new List<TestCase>();
+                _tests = new List<TestCase>(InitialCapacity(_cacheSize));
                 _lastUpdate = DateTime.UtcNow;
 
                 EqtTrace.Verbose("DiscoveryResultCache.AddTest: Notified the onReportTestCases callback.");
             }
         }
     }
+
+    private static int InitialCapacity(long cacheSize) => (int)Math.Min(cacheSize, 512);
 }

src/Microsoft.TestPlatform.CrossPlatEngine/Execution/TestRunCache.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading;
 
@@ -60,12 +59,12 @@ internal class TestRunCache : ITestRunCache
     /// <summary>
     /// The test case currently in progress.
     /// </summary>
-    private ICollection<TestCase> _inProgressTests;
+    private List<TestCase> _inProgressTests;
 
     /// <summary>
     /// Test results buffer
     /// </summary>
-    private ICollection<TestResult> _testResults;
+    private List<TestResult> _testResults;
 
     /// <summary>
     /// Sync object
@@ -93,8 +92,8 @@ internal class TestRunCache : ITestRunCache
         _onCacheHit = onCacheHit;
         _lastUpdate = DateTime.UtcNow;
         _cacheTimeout = cacheTimeout;
-        _inProgressTests = new Collection<TestCase>();
-        _testResults = new Collection<TestResult>();
+        _inProgressTests = new List<TestCase>(InitialCapacity(cacheSize));
+        _testResults = new List<TestResult>(InitialCapacity(cacheSize));
         _runStats = new Dictionary<TestOutcome, long>();
         _syncObject = new object();
 
@@ -272,7 +271,10 @@ internal class TestRunCache : ITestRunCache
         {
             var lastChunk = _testResults;
 
-            _testResults = new Collection<TestResult>();
+            // GetLastChunk() is the end-of-run drain; no further results are expected after this
+            // call, so avoid pre-allocating capacity here. The replacement list exists only to keep
+            // the field non-null and will be allocated lazily if results somehow arrive afterwards.
+            _testResults = new List<TestResult>();
 
             return lastChunk;
         }
@@ -337,8 +339,8 @@ internal class TestRunCache : ITestRunCache
     {
         // Pass on the buffer to the listener and clear the old one
         _onCacheHit(TestRunStatistics, _testResults, _inProgressTests);
-        _testResults = new Collection<TestResult>();
-        _inProgressTests = new Collection<TestCase>();
+        _testResults = new List<TestResult>(InitialCapacity(_cacheSize));
+        _inProgressTests = new List<TestCase>(InitialCapacity(_cacheSize));
         _lastUpdate = DateTime.UtcNow;
 
         // Reset the timer
@@ -370,5 +372,7 @@ internal class TestRunCache : ITestRunCache
             EqtTrace.Warning("TestRunCache: No test found corresponding to testResult '{0}' in inProgress list.", result.DisplayName);
         }
     }
+
+    private static int InitialCapacity(long cacheSize) => (int)Math.Min(cacheSize, 512);
 
 }